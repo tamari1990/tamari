291a7554e11030d975fdf1379cc52c2eccfd777d
S <s>
2023-10-03 12:10:45 +0400

    {"types":"C","runs":11}

--- problem6.java
+++ problem6.java
@@ -1,4 +1,20 @@
+import stanford.karel.SuperKarel;
 
-public class problem6 {
+public class problem6 extends SuperKarel {
 
+	public void run(){
+		move();
+		while(beepersPresent()){
+			pickBeeper();
+		}
+		move();
+		while(beepersPresent()){
+			pickBeeper();
+		}
+		move();
+		while(beepersInBag()){
+			putBeeper();
+		}
+		move();
+	}
 }
