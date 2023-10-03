a90b6372771523f7232dde40edaceae5b1ddd40b
S <s>
2023-10-03 11:44:17 +0400

    {"types":"C","runs":2}

--- problem1.java
+++ problem1.java
@@ -1,4 +1,17 @@
 
-public class problem1 {
+import stanford.karel.SuperKarel;
+
+public class problem1 extends SuperKarel {
+	
+	public void run(){
+		
+		//კარელი დგას 1x1 უჯრაზე. 3x1 უჯრაზე დევს ბრილიანტი, კარელმა ბრილიანტი უნდა
+		//გადაიტანოს 4x1 უჯრაზე და დაამთავროს მუშაობა.
+		move();
+		move();
+		pickBeeper();
+		move();
+		putBeeper();
+	}
 
 }
