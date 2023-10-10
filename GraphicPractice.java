76389863f781070254102d512b9483dcdde998b3
S <s>
2023-10-10 17:27:11 +0400

    {"types":"C","runs":9}

--- GraphicPractice.java
+++ GraphicPractice.java
@@ -3,7 +3,8 @@ import acm.program.GraphicsProgram;
 
 public class GraphicPractice extends GraphicsProgram {
 	public void run (){
-		add(new GOval(100, 200), 100,100);
+		GOval myOval = new GOval(100, 200);
+		add(myOval, 100,100);
 	}
 
 }
