790e978165a51706e7d87c400aa86c7428099f5c
S <s>
2023-10-10 17:36:31 +0400

    {"types":"C","runs":16}

--- GraphicPractice.java
+++ GraphicPractice.java
@@ -5,8 +5,8 @@ public class GraphicPractice extends GraphicsProgram {
 	public void run (){
 		GOval rugbyBall = new GOval(100, 200);
 		add(rugbyBall, 100,100);
-		GOval secondOval = new GOval(100, 200);
-		rugbyBall = new GOval(100,200);
+		//GOval secondOval = new GOval(100, 200);
+		rugbyBall = new GOval(200,200);
 		add(rugbyBall, 200, 200);
 		
 	}
