7d052e81cb9864b71400bed661b63c4593a0baa3
S <s>
2023-10-17 00:13:55 +0400

    {"types":"C","runs":7}

--- problem12.java
+++ problem12.java
@@ -6,22 +6,11 @@ import acm.program.GraphicsProgram;
 public class problem12 extends GraphicsProgram{
 	
 	public void run (){
-//	GOval rugbyBall = new GOval(100, 200);
 	
-//	add(rugbyBall, 100,100);
-//	//GOval secondOval = new GOval(100, 200);
-//	rugbyBall = new GOval(200,200);
-//	add(rugbyBall, 200, 200);
-	
-//	GOval oldBall = rugbyBall;
-//	rugbyBall= new GOval(100, 200, 200, 300);
-	
-//	add(rugbyBall);
-//	rugbyBall.setFilled(true);
-//	rugbyBall.setColor(Color.RED);
-	
-//	oldBall.setFilled(true);	
 	GRect walls = new GRect (300, 180);
 	add(walls, 200, 300);
+	GRect windows = new GRect (30, 60);
+	add(windows, 230, 200);
+    
    }
 }
