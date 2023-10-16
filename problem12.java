12ea4f38c3954369d33f45c8c7a1de2e1713de7e
S <s>
2023-10-17 00:17:06 +0400

    {"types":"C","runs":12}

--- problem12.java
+++ problem12.java
@@ -11,6 +11,7 @@ public class problem12 extends GraphicsProgram{
 	add(walls, 200, 300);
 	GRect door = new GRect (40, 80);
 	add(door, 230, 400);
-    
+    GOval doorknob = new GOval (5,10);
+    add(doorknob, 240,430);
    }
 }
