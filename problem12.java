52b8a726fc33de3968829763fd02425926d663ec
S <s>
2023-10-17 01:06:20 +0400

    {"types":"C","runs":20}

--- problem12.java
+++ problem12.java
@@ -6,16 +6,33 @@ import acm.program.GraphicsProgram;
 public class problem12 extends GraphicsProgram{
 	
 	public void run (){
+	drawWalls();
+	drawTheDoorAndTheDoorKnob();
+	drawWindows();
+	/*
 	
-	GRect walls = new GRect (300, 180);
-	add(walls, 200, 300);
-	GRect door = new GRect (40, 80);
-	add(door, 230, 400);
-    GOval doorknob = new GOval (5,10);
-    add(doorknob, 240,430);
     GRect window = new GRect (30, 60);
     add(window, 300, 310);
     GRect theOtherWindow = new GRect (30, 60);
     add(theOtherWindow, 390,310);
-   }
+   */}
+
+	private void drawWindows() {
+		
+		
+	}
+
+	private void drawTheDoorAndTheDoorKnob() {
+		GRect door = new GRect (40, 80);
+		add(door, 230, 400);
+	    GOval doorknob = new GOval (5,10);
+	    add(doorknob, 240,430);
+		
+	}
+
+	private void drawWalls() {
+		GRect walls = new GRect (300, 180);
+		add(walls, 200, 300);
+		
+	}
 }
