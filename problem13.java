ec804963cef8dfabd9527159a3b8d6790a2ea69a
S <s>
2023-10-18 00:00:02 +0400

    {"types":"C","runs":43}

--- problem13.java
+++ problem13.java
@@ -1,4 +1,12 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class problem13 {
+public class problem13 extends GraphicsProgram {
+	public void run () {
+		double y = getHeight()/2 ;
+		double x = getWidth();
+		GLine horizontalLine = new GLine (0, y, x, y);
+		add (horizontalLine);
+	}
 
 }
