c31fffa61ff7f03cc992928020ffaddd8b7f2426
S <s>
2023-10-18 00:11:20 +0400

    {"types":"C","runs":47}

--- problem15.java
+++ problem15.java
@@ -3,6 +3,7 @@ import acm.program.ConsoleProgram;
 public class problem15 extends ConsoleProgram {
 
 	public void run () {
+		
 		double num1 = readInt("enter first number:");
 		double num2 = readInt("enter second number:");
 		double averageNumberInDoubles = (num1+num2)/2;
