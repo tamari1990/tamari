db3411a251539bcc36a258c7050288cdbe36066d
S <s>
2023-10-19 00:50:11 +0400

    {"types":"C","runs":1}

--- problem17.java
+++ problem17.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class problem17 {
+public class problem17 extends ConsoleProgram  {
+	
+	public void run () {
+		double a = readDouble ("enter real number:");
+		int integerPart = (int) a;
+		double fractionPart = a - integerPart;
+		println("integer part:" + integerPart);
+		println("fraction part:" + fractionPart);
+	}
 
 }
