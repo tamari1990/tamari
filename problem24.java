1e03caa723a0815a4e8bea9dd605b6c6a60b1038
S <s>
2023-10-21 02:16:06 +0400

    {"types":"C","runs":12}

--- problem24.java
+++ problem24.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class problem24 {
+public class problem24 extends ConsoleProgram {
 
+	public void run () {
+
+		int n = readInt ("enter number : ");
+		int amount = 0;
+		for (int i = 0; i <= n; i++){
+			boolean factor = n % i == 0;
+			amount = amount + i;
+			
+		}
+		println(amount);
+	}
 }
