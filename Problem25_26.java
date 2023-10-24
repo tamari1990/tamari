88937c48a83321c9a26ce5f1bfdf9fc9f1977d23
S <s>
2023-10-24 17:41:52 +0400

    {"types":"C","runs":5}

--- Problem25_26.java
+++ Problem25_26.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class Problem25_26 {
+public class Problem25_26 extends ConsoleProgram {
+	
+	public void run() {
+		int n = readInt ("Enter n: ");
+		int m = readInt("Enter m: ");
+		
+		int gcd = gcd(n, m);
+		println("GSD is: " +gcd);
+	}
+
+	private int gcd(int n, int m) {
+		for(int i = Math.min(n, m); i >= 1; i--) {
+			if(n % i == 0 && m % i == 0) {
+				return i;
+			}
+		}
+	    return -1;
+	}
 
 }
