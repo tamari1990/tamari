e7ad0f24c5ba5ee1e9c0065866566fa3d1a0d837
S <s>
2023-10-24 17:31:34 +0400

    {"types":"C","runs":4}

--- problem24.java
+++ problem24.java
@@ -6,7 +6,7 @@ public class problem24 extends ConsoleProgram {
 
 		int n = readInt ("enter number : ");
 		int amount = 0;
-		for (int i = 0; i <= n; i++){
+		for (int i = 1; i <= n; i++){
 			
 			if (n % i == 0) {
 			amount = amount + 1;
