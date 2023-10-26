058fd89782f9884ec93a615067d1cae8d6b93291
S <s>
2023-10-26 17:39:45 +0400

    {"types":"C","runs":3}

--- Problem28.java
+++ Problem28.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class Problem28 {
+public class Problem28 extends ConsoleProgram {
+	
+	public void run() {
+		int n = readInt("enter positive numbers: " );
+		int m = calculateNumberOfEvenNumbers(n);
+		println (m);
+		
+	}
+
+	private int calculateNumberOfEvenNumbers(int n) {
+		int sum = 0;
+		if (n % 2 == 0) {
+			sum++;
+		}
+		return sum;
+	}
+
+	
 
 }
