a72647135d8827305aae7cd14a97b7bc72ddd084
S <s>
2023-10-26 17:43:48 +0400

    {"types":"C","runs":5}

--- Problem28.java
+++ Problem28.java
@@ -4,7 +4,7 @@ public class Problem28 extends ConsoleProgram {
 	
 	public void run() {
 		int n = readInt("enter positive numbers: " );
-		while (n != 1) {
+		while (n != -1) {
 	    n = readInt("enter positive numbers: " );
 		}
 		int m = calculateNumberOfEvenNumbers(n);
