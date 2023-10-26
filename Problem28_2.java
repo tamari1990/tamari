e04a0604d1cca088941327eaad5cb4ac12086265
S <s>
2023-10-26 23:25:58 +0400

    {"types":"C","runs":17}

--- Problem28_2.java
+++ Problem28_2.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Problem28_2 {
+public class Problem28_2 extends ConsoleProgram  {
+
+	public void run () {
+		while(true) {
+			int n = readInt ("enter a positive number: ");
+			int amount = amount(n);
+			println(amount);
+		}
+	}
+
+	private int amount(int n) {
+		int num = 0;
+		if(n % 2 == 0) {
+			num++;
+		}
+		return num;
+	}
 
 }
