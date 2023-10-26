73edcc3487ebe7f0b92de2fd80879a4a703b882a
S <s>
2023-10-27 00:30:03 +0400

    {"types":"C","runs":19}

--- Problem28_2.java
+++ Problem28_2.java
@@ -2,18 +2,30 @@ import acm.program.ConsoleProgram;
 
 public class Problem28_2 extends ConsoleProgram  {
 
-	public void run() {
-	        int num = 0; 
-	        while (true) {
-	            int n = readInt("Enter a positive number: ");
-	            if (n == -1) {
-	                break;
-	            }
-	            if (n % 2 == 0) {
-	                num++;
-	            }
-	        }
 
-	        println("Amount of even numbers: " + num); 
-	    }
-	}
+    public void run() {
+        int amount = countEvenNumbers();
+        println("Amount of even numbers: " + amount);
+    }
+
+    private int countEvenNumbers() {
+        int amount = 0;
+
+        while (true) {
+            int n = readInt("Enter a positive number: ");
+            if (n == -1) {
+                break;
+            }
+            if (n % 2 == 0) {
+                amount++;
+            }
+        }
+
+        return amount;
+    }
+
+   
+}
+
+
+
