ab1809141cf3c53111454faf9f1f1f2d5e2630b2
S <s>
2023-10-30 22:19:58 +0400

    {"types":"C","runs":2}

--- Problem29.java
+++ Problem29.java
@@ -4,22 +4,13 @@ public class Problem29 extends ConsoleProgram {
 	
 	public void  run(){
 		int n = readInt ("enter a number: ");
-		int m = 1;
-		int k = 2;
-		int x = m + k;
-		if(n == 1){
-			println(m);
+		for(int i = 0; i < n; i++){
+		  int a = 1;
+		  int b = 2;
+		  int c = a + b;
+		  b = c;
+		  println (c);
+		  
 		}
-		if(n == 2){
-			println(k);
-		}else{
-		for (int i = 2; i < n ; i++){
-			
-			x += k;
 		}
-		println(x);
-		}
-		
-	}
-
 }
