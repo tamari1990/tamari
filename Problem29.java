8466abdbda3f84e654de9342e81f08feae028666
S <s>
2023-10-30 22:20:51 +0400

    {"types":"C","runs":3}

--- Problem29.java
+++ Problem29.java
@@ -4,12 +4,13 @@ public class Problem29 extends ConsoleProgram {
 	
 	public void  run(){
 		int n = readInt ("enter a number: ");
+		int a = 1;
+		int b = 2;
 		for(int i = 0; i < n; i++){
-		  int a = 1;
-		  int b = 2;
+		  
 		  int c = a + b;
 		  b = c;
-		  println (c);
+		  
 		  
 		}
 		}
