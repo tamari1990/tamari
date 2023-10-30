d455cf0cd7b62052850ca05e6c4a9765a4c5df1f
S <s>
2023-10-30 23:54:19 +0400

    {"types":"C","runs":46}

--- Problem32.java
+++ Problem32.java
@@ -3,15 +3,18 @@ import acm.program.ConsoleProgram;
 public class Problem32 extends ConsoleProgram {
 	
 	public void run() {
-		for(int i = 2; i < 100002; i++){
-			for(int k = 2; k <= i; k++) {
-				if(i % k == 0 && i!=k ){
-					break;
-				}
-				
-			}
-			println(i);
-		}
-	}
+        for (int i = 2; i < 100002; i++) {
+            boolean isPrime = true;
+            for (int k = 2; k * k <= i; k++) {
+                if (i % k == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                println(i);
+            }
+        }
+    }
 
 }
