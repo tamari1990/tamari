e64468cc4941baeff1fb2fe63dc7e43c4ce9ae23
S <s>
2023-10-30 22:53:57 +0400

    {"types":"C","runs":25}

--- Problem30.java
+++ Problem30.java
@@ -6,7 +6,7 @@ public class Problem30 extends ConsoleProgram{
 		int n = readInt("enter a number: ");
 		while(true) {
 			int x = n / 10;
-			int k = n % 10;
+			double k = n % 10;
 			n = x;
 			print(k);
 			
