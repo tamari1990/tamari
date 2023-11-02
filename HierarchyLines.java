6dd189f452777432be69a47491bd428f969d7248
S <s>
2023-11-02 23:12:07 +0400

    {"types":"C","runs":9}

--- HierarchyLines.java
+++ HierarchyLines.java
@@ -1,4 +1,16 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class HierarchyLines {
+public class HierarchyLines extends GraphicsProgram {
+	private static final double distanceBetweenTheAboveAndCentreRects = 120;
+	public void run() {
+	double x1 = getWidth() / 2;
+	double y1 = getHeight() / 2 - distanceBetweenTheAboveAndCentreRects / 2 ;
+	double x2 = x1;
+	double y2 =  getHeight() / 2 + distanceBetweenTheAboveAndCentreRects / 2 ;
+	GLine firstLine = new GLine (x1, y1, x2, y2);
+	add(firstLine);
+	
 
+}
 }
