20cd06e45e570d2e56fff0eddadb2f836e4c25eb
S <s>
2023-11-02 23:18:20 +0400

    {"types":"C","runs":14}

--- HierarchyLines.java
+++ HierarchyLines.java
@@ -9,11 +9,11 @@ public class HierarchyLines extends GraphicsProgram {
 		for(int i = 0; i < 3; i++) {
 	double x1 = getWidth() / 2;
 	double y1 = getHeight() / 2 - distanceBetweenTheAboveAndCentreRects / 2 ;
-	double x2 = getWidth() / 2 - RECT_LENGTH - distanceBetweenTheThreeRects;
+	double x2 = getWidth() / 2 - RECT_LENGTH - distanceBetweenTheThreeRects + i * (RECT_LENGTH + distanceBetweenTheThreeRects);
 	double y2 =  getHeight() / 2 + distanceBetweenTheAboveAndCentreRects / 2 ;
 	GLine firstLine = new GLine (x1, y1, x2, y2);
 	add(firstLine);
-	x2 = x2 + distanceBetweenTheThreeRects + RECT_LENGTH;
+	
 		}
 }
 }
