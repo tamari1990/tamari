b9fea1e2a5cf3c3adf837abb67e04969355f0807
S <s>
2023-11-02 17:28:00 +0400

    {"types":"C","runs":3}

--- Problem34.java
+++ Problem34.java
@@ -7,9 +7,12 @@ import acm.util.RandomGenerator;
 public class Problem34 extends GraphicsProgram{
 	private static final int MIN_RADIUS =10;
 	private static final int MAX_RADIUS =10;
+	private static final int NUM_CIRCLES = 1000;
     private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
-		addRandomCircle();
+		for(int i = 0; i < NUM_CIRCLES; i++) {
+			addRandomCircle();
+		}
 	}
 	
 	private void addRandomCircle() {
