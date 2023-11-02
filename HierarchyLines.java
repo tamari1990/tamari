2dccb1b728fd80aa83f19658b7a03fe7fd6d67c7
S <s>
2023-11-02 23:13:50 +0400

    {"types":"C","runs":10}

--- HierarchyLines.java
+++ HierarchyLines.java
@@ -3,8 +3,10 @@ import acm.program.GraphicsProgram;
 
 public class HierarchyLines extends GraphicsProgram {
 	private static final double distanceBetweenTheAboveAndCentreRects = 120;
+	private static final double RECT_LENGTH = 200;
+	private static final double distanceBetweenTheThreeRects = 40;
 	public void run() {
-	double x1 = getWidth() / 2;
+	double x1 = getWidth() / 2 - RECT_LENGTH - distanceBetweenTheThreeRects;
 	double y1 = getHeight() / 2 - distanceBetweenTheAboveAndCentreRects / 2 ;
 	double x2 = x1;
 	double y2 =  getHeight() / 2 + distanceBetweenTheAboveAndCentreRects / 2 ;
