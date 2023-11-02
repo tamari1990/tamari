0eb60636bd651d2744fcbe671dccac1a77e23400
S <s>
2023-11-02 17:25:48 +0400

    {"types":"C","runs":1}

--- Problem33.java
+++ Problem33.java
@@ -4,9 +4,9 @@ import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class Problem33 extends GraphicsProgram{
-	
-	public void run(){
+public class Problem33 extends GraphicsProgram {
+
+	public void run() {
 		RandomGenerator rgen = new RandomGenerator();
 		RandomOval(rgen);
 	}
@@ -15,7 +15,7 @@ public class Problem33 extends GraphicsProgram{
 		double x = rgen.nextDouble(0, 300);
 		double y = rgen.nextDouble(0, 200);
 		double R = rgen.nextDouble(1, 60);
-		GOval oval = new GOval(x, y, 2 * R, 2 * R );
+		GOval oval = new GOval(x, y, 2 * R, 2 * R);
 		Color color = rgen.nextColor();
 		oval.setFilled(true);
 		oval.setFillColor(color);
--- Problem34.java
+++ Problem34.java
@@ -1,4 +1,27 @@
+import java.awt.Color;
 
-public class Problem34 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
+public class Problem34 extends GraphicsProgram{
+	private static final int MIN_RADIUS =10;
+	private static final int MAX_RADIUS =10;
+    private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		
+	}
+	
+	private void addRandomCircle() {
+		int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
+		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
+		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
+		Color color = rgen.nextColor();
+		
+		GOval circle = new GOval (2 * radius, 2 * radius);
+		circle.setFilled(true);
+		circle.setColor(color);
+		
+		add(circle);
+	}
 }
