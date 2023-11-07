a7c2b4c171f9f893450d0016c77b0bdfa5749755
S <s>
2023-11-07 17:54:42 +0400

    {"types":"C","runs":3}

--- Problem39.java
+++ Problem39.java
@@ -3,16 +3,23 @@ import acm.program.GraphicsProgram;
 
 public class Problem39 extends GraphicsProgram {
 	private static final int R = 50;
+	private static final int PAUSE_MS = 20;
+	private static final int vx = 2;
 	public void run() {
-		drawTheCircle();
+		GOval circle = drawTheCircle();
+		while(circle.getX() <= getWidth()) {
+			int vy = 0;
+			circle.move(vx,vy);
+		}
 	}
     
-	private void drawTheCircle() {
+	private GOval drawTheCircle() {
 		GOval oval = new GOval ( 2 * R , 2 * R);
 		int x = 0; 
 		int y = getHeight() / 2 - R;
 		add(oval, x, y);
 		oval.setFilled(true);
+		return oval;
 	}
 
 }
