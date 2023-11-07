693c31c0b8cbf2dc7b01ec909ecaa3bfd1d896b3
S <s>
2023-11-07 17:46:07 +0400

    {"types":"C","runs":1}

--- Problem39.java
+++ Problem39.java
@@ -1,4 +1,17 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class Problem39 {
+public class Problem39 extends GraphicsProgram {
+	private static final int R = 50;
+	public void run() {
+		drawTheCircle();
+	}
+    
+	private void drawTheCircle() {
+		GOval oval = new GOval ( 2 * R , 2 * R);
+		int x = 0; 
+		int y = getHeight() / 2 - R;
+		add(oval, x, y);
+	}
 
 }
