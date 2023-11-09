42722750d7ca6bba988b0c6a4e435c2a01bedc1c
S <s>
2023-11-09 17:56:54 +0400

    {"types":"C","runs":5}

--- Problem25_26.java
+++ Problem25_26.java
@@ -1,36 +1,36 @@
 import acm.program.ConsoleProgram;
 
 public class Problem25_26 extends ConsoleProgram {
-	
+
 	public void run() {
-		int n = readInt ("Enter n: ");
-		while ( n <= 0) {
+		int n = readInt("Enter n: ");
+		while (n <= 0) {
 			println("n should be > 0");
-			n = readInt ("Enter n:");
+			n = readInt("Enter n:");
 		}
 		int m = readInt("Enter m: ");
-		while ( m <= 0) {
+		while (m <= 0) {
 			println("n should be > 0");
-			m = readInt ("Enter m:");
+			m = readInt("Enter m:");
 		}
-		
+
 		int gcd = gcd(n, m);
-		println("GSD is: " +gcd);
+		println("GSD is: " + gcd);
 		int lcm = lcm(n, m);
-		println("LCM is: " +lcm);
+		println("LCM is: " + lcm);
 	}
 
 	private int lcm(int n, int m) {
-		return n*m / gcd (n, m);
+		return n * m / gcd(n, m);
 	}
 
 	private int gcd(int n, int m) {
-		for(int i = Math.min(n, m); i >= 1; i--) {
-			if(n % i == 0 && m % i == 0) {
+		for (int i = Math.min(n, m); i >= 1; i--) {
+			if (n % i == 0 && m % i == 0) {
 				return i;
 			}
 		}
-	    return -1;
+		return -1;
 	}
 
 }
