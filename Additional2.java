d98323104813d7530c4985c31e9e5d1e5a660361
S <s>
2023-11-09 20:27:16 +0400

    {"types":"C","runs":3}

--- Additional2.java
+++ Additional2.java
@@ -10,15 +10,19 @@ public class Additional2 extends GraphicsProgram {
 	private static final double V_X_MAX = 3;
 	private static final double V_Y_MAX = 3;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
+
 	public void run() {
-		GOval ball = new GOval (2 * RADIUS, 2 * RADIUS);
+		GOval ball = new GOval(2 * RADIUS, 2 * RADIUS);
 		ball.setFilled(true);
 		add(ball, getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
 		double vx = rgen.nextDouble(-V_X_MAX, V_X_MAX);
 		double vy = rgen.nextDouble(-V_Y_MAX, V_Y_MAX);
-		while(true) {
-			ball.move(vx,  vy);
+		while (true) {
+			ball.move(vx, vy);
 			pause(DELAY);
+			if (ball.getY() == getHeight()) {
+				vy = rgen.nextDouble(-V_Y_MAX, 0);
+			}
 		}
 	}
 
