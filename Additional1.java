b0a11f7efc100724b17d6fc864cea5eb3c287aff
S <s>
2023-11-09 17:43:05 +0400

    {"types":"C","runs":2}

--- Additional1.java
+++ Additional1.java
@@ -4,7 +4,7 @@ import acm.program.GraphicsProgram;
 public class Additional1 extends GraphicsProgram{
 	
 	public void run() {
-		GLine line = new GLine (0, getHeight() / 2, getWidth());
+		GLine line = new GLine (0, getHeight() / 2, getWidth(), getHeight() / 2);
 	}
 
 }
