1a64a2a53fa20c67a18ca5ba88a565f94ea9a468
S <s>
2023-11-09 17:56:55 +0400

    {"types":"C","runs":5}

--- Additional1.java
+++ Additional1.java
@@ -1,12 +1,12 @@
 import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
-public class Additional1 extends GraphicsProgram{
-	
+public class Additional1 extends GraphicsProgram {
+
 	public void run() {
-		while(true) {
+		while (true) {
 			removeAll();
-			GLine line = new GLine (0, getHeight() / 2, getWidth(), getHeight() / 2);
+			GLine line = new GLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
 			add(line);
 			pause(20);
 		}
--- Additional2.java
+++ Additional2.java
@@ -1,4 +1,25 @@
+import java.util.Random;
 
-public class Additional2 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class Additional2 extends GraphicsProgram {
+	private static final int RADIUS = 20;
+	private static final int DELAY = 20;
+	private static final double V_X_MAX = 3;
+	private static final double V_Y_MAX = 3;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		GOval ball = new GOval (2 * RADIUS, 2 * RADIUS);
+		ball.setFilled(true);
+		add(ball, getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
+		double vx = rgen.nextDouble(0, V_X_MAX);
+		double vy = rgen.nextDouble(0, V_Y_MAX);
+		while(true) {
+			ball.move(vx,  vy);
+			pause(DELAY);
+		}
+	}
 
 }
