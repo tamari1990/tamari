58cd0a0e44160c4bb57513314ceae3316f452e33
S <s>
2023-11-14 17:59:24 +0400

    {"types":"C","runs":11}

--- Problem44.java
+++ Problem44.java
@@ -1,4 +1,18 @@
+import java.awt.event.MouseEvent;
 
-public class Problem44 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class Problem44 extends GraphicsProgram{
+	GOval circle;
+	private static final int R = 50;
+	public void run() {
+		circle = new GOval (2 * R, 2 * R);
+		circle.setFilled(true);
+		addMouseListeners();
+	}
+
+	public void mouseClicked(MouseEvent e){
+		add(circle, e.getX() - R, e.getY() - R);
+	}
 }
