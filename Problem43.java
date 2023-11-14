3b12df54e3939bbbeb03afbaaac568a00916c4b8
S <s>
2023-11-14 17:41:07 +0400

    {"types":"C","runs":7}

--- Problem43.java
+++ Problem43.java
@@ -1,4 +1,14 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class Problem43 {
+public class Problem43 extends GraphicsProgram {
+	private static final int R = 50;
+	private GOval circle;
+	public void run() {
+		circle = new GOval(2 * R, 2 * R);
+		circle.setFilled(true);
+		add(circle, getWidth() / 2 - R, getHeight() / 2 - R);
+		addMouseListeners();
+	}
 
 }
