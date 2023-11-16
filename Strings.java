662b6b4a9eea2a648e861c5e46f5371c65229256
S <s>
2023-11-16 17:12:33 +0400

    {"types":"C","runs":2}

--- Strings.java
+++ Strings.java
@@ -8,7 +8,7 @@ public class Strings extends ConsoleProgram {
 		String str4 = readLine("enter str4: ");
 		println("1, 2 " + (str1 == str2) + " " + str1.equals(str2));
 		println("1, 3 " + (str1 == str3) + " " + str1.equals(str3));
-		println("1, 4 " + (str1 == str2) + " " + str1.equals(str4));
+		println("1, 4 " + (str1 == str4) + " " + str1.equals(str4));
 	}
 
 }
