c2ddc7882d08ebffcea04c38d62967b264ad9862
S <s>
2023-11-16 17:35:52 +0400

    {"types":"C","runs":4}

--- Problem45.java
+++ Problem45.java
@@ -1,16 +1,17 @@
 import acm.program.ConsoleProgram;
 
 public class Problem45 extends ConsoleProgram {
-	public void run(){
-	String text = readLine("enter a word: ");
-	String symbolText = readLine("Enter symbol: ");
-	char symbol = symbolText.charAt(0);
-	int num = 0;
-	for(int i = 0; i < text.length(); i++) {
-		if(text.charAt(i) == symbol ) {
-			num++;
+	public void run() {
+		String text = readLine("enter a word: ");
+		String symbolText = readLine("Enter symbol: ");
+		char symbol = symbolText.charAt(0);
+		int num = 0;
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == symbol) {
+				num++;
+			}
 		}
-	}
-	println(num);
+		println("number of" + symbol + "is:" + num);
+		
 	}
 }
