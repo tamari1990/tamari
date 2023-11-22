3f91b6e2279e8764068ce11ad66399783e86056c
S <s>
2023-11-23 00:27:11 +0400

    {"types":"C","runs":1}

--- Problem47.java
+++ Problem47.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Problem47 {
+public class Problem47 extends ConsoleProgram {
 
+	public void run() {
+		String word = readLine(" enter a word: ");
+		int matchingChars = 0;
+		for(int i = 0; i < word.length(); i++) {
+			if(word.charAt(i) == word.charAt(word.length() - i)) {
+				matchingChars++;
+		}
+	}
+		if(matchingChars == word.length() / 2){
+			println("This word is a palyndrom");
+		} else{
+			println("This word is not a palyndrom");
+		}
+		
+	}
 }
