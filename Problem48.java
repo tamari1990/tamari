2755e83efe29f672457837e64bec83824440ad48
S <s>
2023-11-23 01:05:29 +0400

    {"types":"C","runs":8}

--- Problem48.java
+++ Problem48.java
@@ -1,4 +1,31 @@
+import org.w3c.dom.Text;
 
-public class Problem48 {
+import acm.program.ConsoleProgram;
+
+public class Problem48 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		for (int i = 0; i < text.length(); i++) {
+			int matches = calculateMatches(text.charAt(i), text, i);
+			println(text.charAt(i) + " : " + matches);
+		}
+	}
+
+	private int calculateMatches(char charAt, String text, int i) {
+		char character = text.charAt(i);
+		int match = 0;
+		for (int j = 0; j < text.length(); j++) {
+			if (character == text.charAt(j)) {
+				match++;
+			}
+		}
+
+		for (int k = i - 1; k > 0; k--) {
+			if (text.charAt(i) == text.charAt(k)) {
+				break;
+			}
+		}
+		return match;
+	}
 
 }
