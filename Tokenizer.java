b461fe7f72ad6463ea61263e160ead647547c42f
S <s>
2023-11-23 23:04:41 +0400

    {"types":"C","runs":10}

--- Tokenizer.java
+++ Tokenizer.java
@@ -1,4 +1,20 @@
+import java.util.StringTokenizer;
 
-public class Tokenizer {
+import acm.program.ConsoleProgram;
+
+public class Tokenizer extends ConsoleProgram{
+	
+	public void run() {
+		String text = readLine("enter text: ");
+		findTheTokens(text);
+	}
+
+	private void findTheTokens(String text) {
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		for(int i = 0; tokenizer.hasMoreTokens(); i++) {
+			println(tokenizer.nextToken());
+		}
+		
+	}
 
 }
