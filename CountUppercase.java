c2953afee07b3f21151145b7e970042ba55700cf
S <s>
2023-11-23 22:04:50 +0400

    {"types":"C","runs":8}

--- CountUppercase.java
+++ CountUppercase.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class CountUppercase {
+public class CountUppercase extends ConsoleProgram {
+	
+	public void run() {
+		String text = readLine("enter text : ");
+		int count = 0;
+		count = calulateTheNumberOfUppercase(text, count);
+		println("There are " + count + "Uppercase letters");
+	}
+
+	private int calulateTheNumberOfUppercase(String text, int count) {
+		for(int i = 0; i < text.length(); i++) {
+			if(Character.isUpperCase(text.charAt(i))) {
+				count++;
+			}
+		}
+		return count;
+	}
 
 }
