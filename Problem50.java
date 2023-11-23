5b6bccd258abad0751e3eeae3327e1fe3ce96a96
S <s>
2023-11-23 23:10:33 +0400

    {"types":"C","runs":13}

--- Problem50.java
+++ Problem50.java
@@ -1,4 +1,24 @@
+import java.util.StringTokenizer;
 
-public class Problem50 {
+import acm.program.ConsoleProgram;
+
+public class Problem50 extends ConsoleProgram{
+	
+	public void run() {
+		String text = readLine("Enter text: ");
+        findTheTokens(text);
+	}
+
+	private void findTheTokens(String text) {
+		StringTokenizer tokenizer = new StringTokenizer(text);
+        int count = 0;
+        while (tokenizer.hasMoreTokens()) {
+            tokenizer.nextToken(); // Move to the next token
+            count++;
+        }
+        println("Number of tokens: " + count);
+    }
+		
+	}
 
 }
