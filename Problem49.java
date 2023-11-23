1eb37bbadcac05f756cdea069919fcc6ff4ef09a
S <s>
2023-11-23 15:34:36 +0400

    {"types":"C","runs":3}

--- Problem49.java
+++ Problem49.java
@@ -1,14 +1,14 @@
 import acm.program.ConsoleProgram;
 
-public class Problem49 extends ConsoleProgram{
+public class Problem49 extends ConsoleProgram {
 	public void run() {
 		String nums = "0123456789";
 		String number = readLine("enter  a number: ");
 		int numInInts = 0;
-		for(int i = 0; i < number.length(); i++) {
-			for(int j = 0; j < nums.length(); j ++) {
-				if(number.charAt(i) == nums.charAt(j)) {
-					numInInts =  numInInts + (j -1) *  (10 ^ number.length() -1); 
+		for (int i = 0; i < number.length(); i++) {
+			for (int j = 0; j < nums.length(); j++) {
+				if (number.charAt(i) == nums.charAt(j)) {
+					numInInts = numInInts + (j - 1) * (int) Math.pow(10, number.length() - 1 - i);
 				}
 			}
 		}
