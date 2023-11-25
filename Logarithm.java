477da186578d29c89537d2017af5e86471fae76c
S <s>
2023-11-26 01:47:40 +0400

    {"types":"C","runs":1}

--- Logarithm.java
+++ Logarithm.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Logarithm {
+public class Logarithm extends ConsoleProgram{
+	
+	public void run() {
+		int m = readInt("Enter the number m: ");
+		int n = readInt("Enter the number n: ");
+		int log = 0;
+		println("logarithm is : " + log);
+		for(int i = 0; i < m; i++) {
+			if(Math.pow(n, i) <= m) {
+				log = i;
+			}
+		}
+	}
 
 }
