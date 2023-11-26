8871804cfbf66d00e521b92d0e2c9c59ffe3d71c
S <s>
2023-11-26 11:50:35 +0400

    {"types":"C","runs":2}

--- FatherTest.java
+++ FatherTest.java
@@ -7,7 +7,7 @@ public class FatherTest extends ConsoleProgram {
 		int matches = 0;
 		for (int i = 0; i < luka.length(); i++) {
 			int colliders = calculateTheMathes(luka, anakin, matches, i);
-			println(matches);
+			println(colliders);
 
 		}
 
@@ -15,7 +15,7 @@ public class FatherTest extends ConsoleProgram {
 
 	private int calculateTheMathes(String luka, String anakin, int matches, int i) {
 		int match = 0;
-		for (int j = i; j < luka.length(); j++) {
+		for (int j = i; j <= Math.min(luka.length(), anakin.length()); j++) {
 			if (luka.charAt(j) == anakin.charAt(j)) {
 				match++;
 			} else {
