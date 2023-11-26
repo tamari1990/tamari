ddb1fe7e037ef4b9875be55395867b30f2a3a4a9
S <s>
2023-11-26 11:45:43 +0400

    {"types":"C","runs":1}

--- FatherTest.java
+++ FatherTest.java
@@ -1,4 +1,30 @@
+import acm.program.ConsoleProgram;
 
-public class FatherTest {
+public class FatherTest extends ConsoleProgram {
+	public void run() {
+		String luka = readLine("Enter Luka's DNA: ");
+		String anakin = readLine("Enter Anakin's DNA: ");
+		int matches = 0;
+		for (int i = 0; i < luka.length(); i++) {
+			int colliders = calculateTheMathes(luka, anakin, matches, i);
+			println(matches);
 
+		}
+
+	}
+
+	private int calculateTheMathes(String luka, String anakin, int matches, int i) {
+		int match = 0;
+		for (int j = i; j < luka.length(); j++) {
+			if (luka.charAt(j) == anakin.charAt(j)) {
+				match++;
+			} else {
+				break;
+			}
+		}
+		if(match > matches){
+		matches = match;		
+		}
+		return matches;
+	}
 }
