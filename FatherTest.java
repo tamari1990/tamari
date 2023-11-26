58433e71ca1ed5e8cea7bd568e677adb05edfc91
S <s>
2023-11-26 12:05:29 +0400

    {"types":"C","runs":7}

--- FatherTest.java
+++ FatherTest.java
@@ -12,10 +12,18 @@ public class FatherTest extends ConsoleProgram {
 	            }
 
 		}
-		println(matches);
+		boolean isSon = isSon(matches, luka.length());
+		if(isSon) {
+			println("Luka is Anakin's son");
+		} else{
+			println("Anakin is not the father");
+		}
 
 	}
-
+	private boolean isSon (int matches, int lukaLength) {
+        return matches > lukaLength / 2;
+    }
+	
 	private int calculateTheMathes(String luka, String anakin, int matches, int i) {
 		int match = 0;
 		for (int j = i; j < Math.min(luka.length(), anakin.length()); j++) {
