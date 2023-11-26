6ad67aba290fd466318da62866eb0f18adcf1b57
S <s>
2023-11-26 14:02:19 +0400

    {"types":"C","runs":9}

--- Zigzag.java
+++ Zigzag.java
@@ -1,4 +1,12 @@
-
-public class Zigzag {
+import acm.program.GraphicsProgram;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+public class Zigzag extends GraphicsProgram{
+	
+	public void run() {
+		GPoint firstPoint = new GPoint (0 , 0);
+		addMouseListeners();
+	}
 
 }
