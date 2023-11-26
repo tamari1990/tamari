9b5e1d15d11ba2e7d5a213a2b0767c1bf27757cf
S <s>
2023-11-27 01:06:44 +0400

    {"types":"C","runs":5}

--- ScoreCounter.java
+++ ScoreCounter.java
@@ -1,4 +1,18 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class ScoreCounter {
+public class ScoreCounter extends GraphicsProgram{
+	private static final int BOX_SIZE = 70;
+	public void run() {
+		drawTheSquare();
+	}
+
+	private void drawTheSquare() {
+		 GRect square = new GRect(BOX_SIZE, BOX_SIZE);
+		 int x = getWidth() / 2 - BOX_SIZE / 2;
+		 int y = getHeight() / 2 - BOX_SIZE / 2;
+		 add(square, x, y);
+		
+	}
 
 }
