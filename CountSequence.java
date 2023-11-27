ae2f38e3a9eb616099e9c4a3a8f36f324c7a51b8
S <s>
2023-11-28 01:15:45 +0400

    {"types":"C","runs":33}

--- CountSequence.java
+++ CountSequence.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class CountSequence {
+public class CountSequence extends ConsoleProgram{
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int NUMBER_OF_EXPERIMENTS = 1000;
+	public void run() {
+		int number = n;
+	}
 
 }
