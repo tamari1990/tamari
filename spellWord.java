1f0fd4800891c70fc913e65f46759aad6e57f5ed
S <s>
2023-11-27 21:33:18 +0400

    {"types":"C","runs":26}

--- spellWord.java
+++ spellWord.java
@@ -1,4 +1,37 @@
+import acm.program.ConsoleProgram;
 
-public class spellWord {
+public class spellWord extends ConsoleProgram{
+	
+	public void run() {
+		String firstWord = readLine("enter the first word: ");
+		String secondWord = readLine("enter the second word: ");
+		boolean isPossible = possible(firstWord, secondWord);
+		if(possible(firstWord, secondWord)){
+			println("true");
+		}
+	}
+
+	private boolean possible(String firstWord, String secondWord) {
+		int num = 0;
+		for(int i = 0; i < secondWord.length(); i++) {
+			boolean isCharInFirstWord = isCharacter(i, firstWord, secondWord);
+			if(isCharacter(i, firstWord, secondWord)){
+				num++;
+			}
+		}
+		if(num == secondWord.length()){
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isCharacter(int i, String firstWord, String secondWord) {
+		for(int j = 0; j < firstWord.length(); j++) {
+			if(secondWord.charAt(i) == firstWord.charAt(j)) {
+				return true;
+			}
+		} 
+		return false;
+	}
 
 }
