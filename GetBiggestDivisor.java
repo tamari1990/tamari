e36f51f549594e2e53130e36555b571f9b838d1e
S <s>
2023-11-27 21:08:13 +0400

    {"types":"C","runs":20}

--- GetBiggestDivisor.java
+++ GetBiggestDivisor.java
@@ -10,7 +10,7 @@ public class GetBiggestDivisor extends ConsoleProgram {
 
 	private int biggestDivisor(int n) {
 		int x = 2;
-		for (int i = 1; i <= n; i++) {
+		for (int i = 2; i <= n; i++) {
 			boolean isPrime = isPrime(i);
 			if (n % i == 0 &&  isPrime) {
 				x = i;
