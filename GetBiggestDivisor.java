394abea21ba15a4731035f343a9d77ff5d24232a
S <s>
2023-11-27 21:18:03 +0400

    {"types":"C","runs":24}

--- GetBiggestDivisor.java
+++ GetBiggestDivisor.java
@@ -23,17 +23,15 @@ public class GetBiggestDivisor extends ConsoleProgram {
 
 	}
 
-	private boolean isPrime(int i) {
-		for (int j = 2; j <= i; j++) {
-			if (i <= 1) {
-				return false;
-			}
-			if (i % j == 0) {
-				return false;
-			}
-		}
-		return true;
-
+	private boolean isPrime(int num) {
+	    if (num <= 1) {
+	        return false;
+	    }
+	    for (int i = 2; i <= Math.sqrt(num); i++) {
+	        if (num % i == 0) {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
-
 }
