882972a75bde8612fe12875ac24a82b73c05583c
S <s>
2023-11-28 01:24:45 +0400

    {"types":"C","runs":34}

--- CountSequence.java
+++ CountSequence.java
@@ -1,11 +1,33 @@
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
-public class CountSequence extends ConsoleProgram{
+public class CountSequence extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int NUMBER_OF_EXPERIMENTS = 1000;
+
 	public void run() {
-		int number = n;
+		int n = readInt("enter a positive integer: ");
+		int sum = 0;
+		for (int i = 0; i < NUMBER_OF_EXPERIMENTS; i++) {
+			int numOfSteps = flipTheCoin(n);
+			sum = sum + numOfSteps;
+		}
+		println(sum / NUMBER_OF_EXPERIMENTS);
+	}
+
+	private int flipTheCoin(int n) {
+		int numOfSteps = 0;
+		boolean isHeads = rgen.nextBoolean();
+		while (n != 0) {
+			if (isHeads) {
+				n = n / 2;
+				numOfSteps++;
+			} else {
+				n = n - 1;
+				numOfSteps++;
+			}
+		}
+		return 0;
 	}
 
 }
