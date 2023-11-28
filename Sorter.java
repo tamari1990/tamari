683e32831cad417d9affa80e26ddbd79c1182ee9
S <s>
2023-11-29 00:14:55 +0400

    {"types":"C","runs":25}

--- Sorter.java
+++ Sorter.java
@@ -20,7 +20,7 @@ public class Sorter extends ConsoleProgram {
 				for (int j = i; j >= 0; j--) {
 					if (s.charAt(j) == ' ') {
 						newSentence = newSentence + s.substring(j, i + 1);
-						break;
+						continue;
 					}
 				}
 				
