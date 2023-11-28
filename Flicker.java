a4244cbcd4c9360fa8993f7b386b0733c9e57fb4
S <s>
2023-11-29 01:21:16 +0400

    {"types":"C","runs":46}

--- Flicker.java
+++ Flicker.java
@@ -29,6 +29,7 @@ public class Flicker extends GraphicsProgram {
 				while(colorNumber != 5) {
 				colorNumber = rgen.nextInt(2, 5);
 				}
+				pause(1000);
 			}
 			if (colorNumber == 2) {
 				circle.setColor(Color.blue);
