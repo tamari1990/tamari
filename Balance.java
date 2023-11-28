2873a57d5b101e6b834386806dbbb8c86df65b6f
S <s>
2023-11-28 22:39:34 +0400

    {"types":"C","runs":4}

--- Balance.java
+++ Balance.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class Balance {
+public class Balance extends ConsoleProgram {
+	public void run() {
+		String s = ("enter the text: ");
+		int matches = 0;
+		matches = countTheMatches(s, matches);
+		println(matches);
+	}
 
+	private int countTheMatches(String s, int matches) {
+		int amountOfR = 0;
+		int amountOfL = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'R') {
+				amountOfR++;
+			}
+			if (s.charAt(i) == 'L') {
+				amountOfL++;
+			}
+			if (amountOfR == amountOfL) {
+				matches++;
+			}
+		}
+		return matches;
+	}
 }
