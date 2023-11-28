c26b484f565f1b4accf8cf8a57494843ae7b78d9
S <s>
2023-11-28 16:23:23 +0400

    {"types":"C","runs":4}

--- Pangram.java
+++ Pangram.java
@@ -1,35 +1,33 @@
 import acm.program.ConsoleProgram;
 
-public class Pangram extends ConsoleProgram{
+public class Pangram extends ConsoleProgram {
 	private String sentence;
+
 	public void run() {
 		sentence = readLine("enter a sentence: ");
 		checkIfPangram(sentence);
 		println(checkIfPangram(sentence));
 	}
+
 	public boolean checkIfPangram(String sentence) {
-		for(int i = 'a'; i <= 'z'; i++) {
-			
-			if(checkIfSymbolExists(i)){
-				break;
-			} else{
+		for (int i = 'a'; i <= 'z'; i++) {
+
+			if (!checkIfSymbolExists(i)) {
 				return false;
 			}
-			
+
 		}
-		
+
 		return true;
 	}
+
 	public boolean checkIfSymbolExists(int i) {
-		for(int j = 0; j < sentence.length(); j++) {
-			if(sentence.charAt(j) == i) {
-				break;
-			} else{
-				return false;
+		for (int j = 0; j < sentence.length(); j++) {
+			if (sentence.charAt(j) == i) {
+				return true;
 			}
 		}
-		return true;
+		return false;
 	}
-	
 
 }
