474a27d95891eb8bc221c9d18b823f2de5346397
S <s>
2023-11-28 22:18:37 +0400

    {"types":"C","runs":1}

--- IPadress.java
+++ IPadress.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class IPadress {
+public class IPadress extends ConsoleProgram{
+	
+	public void run(){
+		String address = readLine("enter IP address");
+		address = replaceDots(address);
+		println(address);
+	}
+
+	private String replaceDots(String address) {
+		String address2 = address.replace(".", "[.]");
+		return address2;
+	}
 
 }
