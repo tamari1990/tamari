862ff90aec569eff1c095eac0072e21b38e52068
S <s>
2023-11-28 23:58:49 +0400

    {"types":"C","runs":11}

--- Sorter.java
+++ Sorter.java
@@ -4,15 +4,16 @@ public class Sorter extends ConsoleProgram {
 	private int num;
 	public void run() {
 		String s = readLine("enter text: ");
+		num = 0;
 		s = sortTheSentence(s);
 		println(s);
 	}
 
 	private String sortTheSentence(String s) {
-		num = 1;
 		String number = "" + num;
 		char index = number.charAt(0);
 		String newSentence = "";
+		num++;
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) == index) {
 				for (int j = i; j > 0; j--) {
@@ -23,7 +24,6 @@ public class Sorter extends ConsoleProgram {
 				}
 
 			}
-			num++;
 		}
 		return newSentence;
 	}
