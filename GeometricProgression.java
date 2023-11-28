1acca909c9ab03d3316827366638f83f27fe6681
S <s>
2023-11-29 02:29:48 +0400

    {"types":"C","runs":58}

--- GeometricProgression.java
+++ GeometricProgression.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class GeometricProgression {
+public class GeometricProgression extends ConsoleProgram{
+	
+	public void run() {
+		String s = "";
+		while(true) {
+			int n = readInt("enter an integer: ");
+			s = s + n;
+			if(n == - 1) {
+				break;
+			}
+		}
+		println(s);
+	}
 
 }
