8eea06945344018aa71e6f054821b19972714797
S <s>
2023-11-28 23:55:00 +0400

    {"types":"C","runs":8}

--- Sorter.java
+++ Sorter.java
@@ -1,4 +1,30 @@
+import acm.program.ConsoleProgram;
 
-public class Sorter {
+public class Sorter extends ConsoleProgram {
+	private int num;
+	public void run() {
+		String s = readLine("enter text: ");
+		s = sortTheSentence(s);
+		println(s);
+	}
 
+	private String sortTheSentence(String s) {
+		num = 0;
+		String number = "" + num;
+		char index = number.charAt(0);
+		String newSentence = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == index) {
+				for (int j = i; j > 0; j--) {
+					if (s.charAt(j) == ' ') {
+						newSentence = s.substring(j + 1, i + 1) + " " + newSentence;
+
+					}
+				}
+
+			}
+			num++;
+		}
+		return newSentence;
+	}
 }
