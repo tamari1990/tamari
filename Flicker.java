69183c106beba7860be868561babc6e0fbb3a4d6
S <s>
2023-11-29 00:54:19 +0400

    {"types":"C","runs":30}

--- Flicker.java
+++ Flicker.java
@@ -1,9 +1,12 @@
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class Flicker extends GraphicsProgram{
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int CIRCLE_D = 100;
 	public void run() {
 		addMouseListeners();
@@ -13,6 +16,23 @@ public class Flicker extends GraphicsProgram{
 		int y = e.getY();
 		GOval circle = new GOval (CIRCLE_D, CIRCLE_D);
 		add(circle, x - CIRCLE_D / 2 , y - CIRCLE_D / 2);
+		circle.setFilled(true);
+		int colorNumber = rgen.nextInt(1, 5);
+		if(colorNumber == 1) {
+			circle.setColor(Color.red);
+		}
+		if(colorNumber == 2) {
+			circle.setColor(Color.blue);
+		}
+		if(colorNumber == 3) {
+			circle.setColor(Color.yellow);
+		}
+		if(colorNumber == 4) {
+			circle.setColor(Color.black);
+		}
+		if(colorNumber == 4) {
+			circle.setColor(Color.red);
+		}
 	}
 
 }
