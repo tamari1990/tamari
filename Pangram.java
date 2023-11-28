7ecd829ba9291d2a29427a271220a99646fba807
S <s>
2023-11-28 16:19:38 +0400

    {"types":"C","runs":1}

--- Pangram.java
+++ Pangram.java
@@ -1,4 +1,34 @@
+import acm.program.ConsoleProgram;
 
-public class Pangram {
+public class Pangram extends ConsoleProgram{
+	private String sentence;
+	public void run() {
+		sentence = readLine("enter a sentence: ");
+		checkIfPangram(sentence);
+	}
+	public boolean checkIfPangram(String sentence) {
+		for(int i = 'a'; i <= 'z'; i++) {
+			
+			if(checkIfSymbolExists(i)){
+				break;
+			} else{
+				return false;
+			}
+			
+		}
+		
+		return true;
+	}
+	public boolean checkIfSymbolExists(int i) {
+		for(int j = 0; j < sentence.length(); j++) {
+			if(sentence.charAt(j) == i) {
+				break;
+			} else{
+				return false;
+			}
+		}
+		return true;
+	}
+	
 
 }
