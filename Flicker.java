9baf900614259e4e558ce264a6464bffaa395140
S <s>
2023-11-29 00:39:04 +0400

    {"types":"C","runs":29}

--- Flicker.java
+++ Flicker.java
@@ -1,4 +1,18 @@
+import java.awt.event.MouseEvent;
 
-public class Flicker {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+public class Flicker extends GraphicsProgram{
+	private static final int CIRCLE_D = 100;
+	public void run() {
+		addMouseListeners();
+	}
+	public void mouseClicked (MouseEvent e){
+		int x = e.getX();
+		int y = e.getY();
+		GOval circle = new GOval (CIRCLE_D, CIRCLE_D);
+		add(circle, x - CIRCLE_D / 2 , y - CIRCLE_D / 2);
+	}
 
 }
