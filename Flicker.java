2a07bfb9189f966632497be14d12baafd9e191e1
S <s>
2023-11-29 01:05:23 +0400

    {"types":"C","runs":39}

--- Flicker.java
+++ Flicker.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
@@ -9,6 +10,7 @@ public class Flicker extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int CIRCLE_D = 100;
 	private int colorNumber;
+
 	public void run() {
 		addMouseListeners();
 	}
@@ -19,30 +21,32 @@ public class Flicker extends GraphicsProgram {
 		GOval circle = new GOval(CIRCLE_D, CIRCLE_D);
 		add(circle, x - CIRCLE_D / 2, y - CIRCLE_D / 2);
 		circle.setFilled(true);
-		while (colorNumber != 5) {
-			colorNumber = rgen.nextInt(1, 5);
-			if (colorNumber == 1) {
-				circle.setColor(Color.red);
-				pause(1000);
-			}
-			if (colorNumber == 2) {
-				circle.setColor(Color.blue);
-				pause(1000);
-			}
-			if (colorNumber == 3) {
-				circle.setColor(Color.yellow);
-				pause(1000);
-			}
-			if (colorNumber == 4) {
-				circle.setColor(Color.black);
-				pause(1000);
-			}
-			if (colorNumber == 5) {
-				circle.setColor(Color.green);
-				pause(1000);
+		GObject object = getElementAt(e.getX(), e.getY());
+		if (object != null) {
+			while (colorNumber != 5) {
+				colorNumber = rgen.nextInt(1, 5);
+				if (colorNumber == 1) {
+					circle.setColor(Color.red);
+					pause(1000);
+				}
+				if (colorNumber == 2) {
+					circle.setColor(Color.blue);
+					pause(1000);
+				}
+				if (colorNumber == 3) {
+					circle.setColor(Color.yellow);
+					pause(1000);
+				}
+				if (colorNumber == 4) {
+					circle.setColor(Color.black);
+					pause(1000);
+				}
+				if (colorNumber == 5) {
+					circle.setColor(Color.green);
+					pause(1000);
+				}
 			}
 		}
 	}
-	
 
 }
