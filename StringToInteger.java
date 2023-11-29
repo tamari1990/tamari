ae3b5de8765571c89f3123e9de065714ca5dd15f
S <s>
2023-11-29 23:27:58 +0400

    {"types":"C","runs":39}

--- StringToInteger.java
+++ StringToInteger.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class StringToInteger {
+public class StringToInteger extends ConsoleProgram{
+	
+	public void run() {
+		String s = readLine("enter a number: ");
+		int num = 0;
+		String s2 = "" + num;
+		while(!s.equals(s2)) {
+			num++;
+		}
+		println(num);
+	}
 
 }
