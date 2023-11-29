3e7e3f978797e70e55a02541e58f0983b794a98b
S <s>
2023-11-29 21:37:34 +0400

    {"types":"C","runs":6}

--- Sorter.java
+++ Sorter.java
@@ -1,30 +1,33 @@
 import acm.program.ConsoleProgram;
+import acm.program.ConsoleProgram;
 
 public class Sorter extends ConsoleProgram {
-	private int num;
+    private int num;
 
-	public void run() {
-		String s = readLine("enter text: ");
-		s = sortTheSentence(s);
-		println(s);
-	}
+    public void run() {
+        String s = readLine("enter text: ");
+        s = sortTheSentence(s);
+        println(s);
+    }
 
-	private String sortTheSentence(String s) {
-		int num = 1;
-		String number = "" + num;
-		char index = number.charAt(0);
-		String newSentence = "";
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == index) {
-				num++;
-				for (int j = i; j >= 0; j--) {
-					if (s.charAt(j) == ' ') {
-						newSentence = newSentence + s.substring(j, i + 1);					
-					} 
-				}
-				
-			}
-		}
-		return newSentence;
-	}
+    private String sortTheSentence(String s) {
+        int num = 1;
+        String number = "" + num;
+        char index = number.charAt(0);
+        String newSentence = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = Character.getNumericValue(s.charAt(i));
+                for (int j = i; j >= 0; j--) {
+                    if (s.charAt(j) == ' ' || j == 0) {
+                        newSentence = s.substring(j, i + 1) + newSentence;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return newSentence.trim(); // Trim to remove leading space
+    }
 }
