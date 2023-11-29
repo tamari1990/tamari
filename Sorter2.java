0319e6490ff2eca20a5b3873326d64748088dff3
S <s>
2023-11-29 22:47:35 +0400

    {"types":"C","runs":14}

--- Sorter2.java
+++ Sorter2.java
@@ -1,4 +1,31 @@
 
-public class Sorter2 {
+import acm.program.ConsoleProgram;
 
+public class Sorter2 extends ConsoleProgram {
+	private int num;
+
+	public void run() {
+		String s = readLine("enter text: ");
+		num = 0;
+		s = sortTheSentence(s);
+		println(s);
+	}
+
+	private String sortTheSentence(String s) {
+		num++;
+		String number = "" + num;
+		char index = number.charAt(0);
+		String newSentence = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == index) {
+				for (int j = i; j >= 0; j--) {
+					if (s.charAt(j) == ' ') {
+						newSentence = newSentence + s.substring(j, i + 1);					
+					} 
+				}
+				
+			}
+		}
+		return newSentence;
+	}
 }
