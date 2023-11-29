63146843e2cde91b6cd7eb61f814f416b1df0bd7
S <s>
2023-11-30 01:17:45 +0400

    {"types":"C","runs":85}

--- removeNplet.java
+++ removeNplet.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class removeNplet {
+public class removeNplet extends ConsoleProgram {
+
+	public void run() {
+		String s = readLine("enter a text: ");
+		int n = readInt("enter n: ");
+		String s2 = removeNplets(s, n);
+		println(s2);
+	}
+
+	private String removeNplets(String s, int n) {
+		String s0 = "";
+		for (int i = 0; i < s.length(); i++) {
+			int k = 1;
+			int numOfMatches = 0;
+			while (k != n - 1) {
+				if (s.charAt(i + k) == s.charAt(i)) {
+					numOfMatches++;
+					k++;
+					s0 = s0 + s.substring(0, i) + s.substring(i + k, s.length());
+				}
+			}
+		}
+		return s0;
+	}
 
 }
