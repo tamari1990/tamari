c15cbbb27bf10748359fe7e0a332ba13f1751283
S <s>
2023-11-29 12:06:49 +0400

    {"types":"C","runs":1}

--- Print1.java
+++ Print1.java
@@ -1,4 +1,21 @@
+import acm.graphics.*;
+import acm.program.ConsoleProgram;
 
-public class Print1 {
+public class Print1 extends ConsoleProgram {
+	public void run() {
+		int x = 1;
+		int y = 2;
+		GObject o = new GRect(x, y);
+		String s = "shalaxo";
+		println(iseRa(x, y, o, s));
+		println(iseRa(x, y, o, s));
+	}
 
-}
+	private int iseRa(int x, double y, GObject r, String s){
+	x++;
+	y += 1;
+	r.move(x,y);
+	s = s.substring(s.indexOf('a'));
+	return (int)(x + y + r.getX() + s.length());
+	}
+	}
