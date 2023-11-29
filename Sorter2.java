2c64370456dd746b50151c7356e159e51e032578
S <s>
2023-11-29 23:19:09 +0400

    {"types":"C","runs":37}

--- Sorter2.java
+++ Sorter2.java
@@ -25,12 +25,12 @@ public class Sorter2 extends ConsoleProgram {
 						newSentence = newSentence + s.substring(j, i + 1);
 						break;
 					} 
-					if(j == 0 && s.charAt(0) != ' ') {
-						newSentence = newSentence + s.substring(0, i + 1);
-					}
+					
 					
 				}
-				
+				if (i == 0 && s.charAt(0) != ' ') {
+                    newSentence = s.substring(0, i + 1) + newSentence;
+                }
 			}
 		}
 		return newSentence;
