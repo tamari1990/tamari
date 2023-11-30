50f1ff3d03cbc2a348b54f87a0615e8c4be8d29e
S <s>
2023-11-30 23:16:48 +0400

    {"types":"C","runs":102}

--- Green.java
+++ Green.java
@@ -11,17 +11,18 @@ public class Green extends GraphicsProgram {
 	private final static int DELAY = 2000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private GOval circle;
+
 	public void run() {
 		addMouseListeners();
-		for(int i = 0; i < CIRCLE_NUM; i++) {
-			circle = new GOval (CIRCLE_D, CIRCLE_D);
+		for (int i = 0; i < CIRCLE_NUM; i++) {
+			circle = new GOval(CIRCLE_D, CIRCLE_D);
 			double x = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double y = rgen.nextDouble(0, getHeight() - CIRCLE_D);
 			circle.setFilled(true);
 			add(circle, x, y);
 		}
-		while(true) {
-			circle = new GOval (CIRCLE_D, CIRCLE_D);
+		while (true) {
+			circle = new GOval(CIRCLE_D, CIRCLE_D);
 			double x = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double y = rgen.nextDouble(0, getHeight() - CIRCLE_D);
 			circle.setFilled(true);
@@ -29,14 +30,18 @@ public class Green extends GraphicsProgram {
 			pause(DELAY);
 		}
 	}
+
 	public void mouseClicked(MouseEvent e) {
-		for(int i = 0; i < 3; i++) {
+		int matches = 0;
+		GObject obj = getElementAt(e.getX(), e.getY());
+		for (int i = 0; i < 2; i++) {
 			int x = e.getX();
 			int y = e.getY();
-			GObject obj = getElementAt(x, y);
-			if(obj != circle) {
-				break;
+			if (getElementAt(x, y) == obj) {
+				matches++;
 			}
+		}
+		if(matches == 3) {
 			remove(obj);
 		}
 	}
