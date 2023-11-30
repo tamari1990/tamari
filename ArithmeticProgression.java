8ef0783ee126f4bdf01892f390403830ee72f081
S <s>
2023-12-01 00:53:56 +0400

    {"types":"C","runs":119}

--- ArithmeticProgression.java
+++ ArithmeticProgression.java
@@ -1,23 +1,21 @@
 import acm.program.ConsoleProgram;
 
-public class ArithmeticProgression extends ConsoleProgram{
-	
+public class ArithmeticProgression extends ConsoleProgram {
+
 	public void run() {
 		int a1 = readInt("enter an integer: ");
 		int a2 = readInt("enter an integer: ");
 		int d = a2 - a1;
-		while(true) {
+		while (true) {
 			int a = readInt("enter an integer: ");
-			if(a - a2 == d) {
-				a2 = a;
-				continue;
-			}	else{
-				println("no");
-			}
-			if(a == -1) {
+			if (a == -1) {
 				break;
 			}
-		}
+			if (a - a2 != d) {
+				a2 = a;
+				continue;
+			}
+		}  println("no");
 	}
 
 }
