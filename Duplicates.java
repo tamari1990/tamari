f9a18e55087ba90bb6234be8039006c340a16333
S <s>
2023-12-01 02:34:29 +0400

    {"types":"C","runs":136}

--- Duplicates.java
+++ Duplicates.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class Duplicates {
+public class Duplicates extends ConsoleProgram{
+	
+	public void run() {
+		String s = readLine("enter text: ");
+		removeDuplicates(s);
+	}
+	private void removeDuplicates(String s) {
+		String s2 = "";
+		for(int i = 0; i < s.length(); i++) {
+			int j = 1;
+			while(s.charAt(i) != s.charAt(i + j)) {
+				j++;
+				s2 = s2 + s.substring(0, i) + s.substring(j, s.length());
+			}
+		}
+		println(s2);
+	}
+
+	
 
 }
