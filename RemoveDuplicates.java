1e00496f15d969114334a1c79163549c22a70820
S <s>
2023-12-01 02:27:37 +0400

    {"types":"C","runs":130}

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -8,14 +8,15 @@ public class RemoveDuplicates extends ConsoleProgram{
 	}
 
 	private void removeDuplicates(String s) {
+		String s2 = "";
 		for(int i = 0; i < s.length(); i++) {
 			int j = 0;
 			while(s.charAt(i) != s.charAt(i + j)) {
 				j++;
-				s = s.substring(0, i) + s.substring(j, s.length());
+				s2 = s2 + s.substring(0, i) + s.substring(j, s.length());
 			}
 		}
-		println(s);
+		println(s2);
 	}
 
 	
