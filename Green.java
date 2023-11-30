031e49f353b0a9048bdd5cd53dc120092423f722
S <s>
2023-11-30 22:56:18 +0400

    {"types":"C","runs":97}

--- Green.java
+++ Green.java
@@ -13,8 +13,8 @@ public class Green extends GraphicsProgram {
 			GOval circle = new GOval (CIRCLE_D, CIRCLE_D);
 			double x = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double y = rgen.nextDouble(0, getHeight() - CIRCLE_D);
+			circle.setFilled(true);
 			add(circle, x, y);
-			pause(DELAY);
 		}
 	}
 
