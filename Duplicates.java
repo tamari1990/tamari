0fbec9117c04cebdb133af8d15d7e5003225afb9
S <s>
2023-12-01 02:35:25 +0400

    {"types":"C","runs":137}

--- Duplicates.java
+++ Duplicates.java
@@ -1,23 +1,28 @@
 import acm.program.ConsoleProgram;
 
-public class Duplicates extends ConsoleProgram{
-	
-	public void run() {
-		String s = readLine("enter text: ");
-		removeDuplicates(s);
-	}
-	private void removeDuplicates(String s) {
-		String s2 = "";
-		for(int i = 0; i < s.length(); i++) {
-			int j = 1;
-			while(s.charAt(i) != s.charAt(i + j)) {
-				j++;
-				s2 = s2 + s.substring(0, i) + s.substring(j, s.length());
-			}
-		}
-		println(s2);
-	}
+public class Duplicates extends ConsoleProgram {
 
-	
+    public void run() {
+        String s = readLine("Enter text: ");
+        removeDuplicates(s);
+    }
 
+    private void removeDuplicates(String s) {
+        String s2 = "";
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            int j = 1;
+
+            // Check if the current character is not equal to the next character
+            while (i + j < s.length() && currentChar != s.charAt(i + j)) {
+                j++;
+            }
+
+            // If j reached the end or found a different character, append to s2
+            if (i + j == s.length()) {
+                s2 += currentChar;
+            }
+        }
+        println(s2);
+    }
 }
