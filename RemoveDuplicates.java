e702f1553e6027cdfdae260caf41f1a3a7c225b7
S <s>
2023-12-01 02:30:58 +0400

    {"types":"C","runs":133}

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -6,15 +6,14 @@ public class RemoveDuplicates extends ConsoleProgram{
 		String s = readLine("enter text: ");
 		removeDuplicates(s);
 	}
-
+	//abbg
 	private void removeDuplicates(String s) {
 		String s2 = "";
 		for(int i = 0; i < s.length(); i++) {
-			int j = 0;
+			int j = 1;
 			while(s.charAt(i) != s.charAt(i + j)) {
 				j++;
 				s2 = s2 + s.substring(0, i) + s.substring(j, s.length());
-				println(s2);
 			}
 		}
 		println(s2);
