eac91e1b22f1659032db49f07e25ec3e30b4921a
S <s>
2023-11-30 23:05:34 +0400

    {"types":"C","runs":100}

--- Green.java
+++ Green.java
@@ -1,3 +1,6 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
@@ -7,22 +10,35 @@ public class Green extends GraphicsProgram {
 	private final static int CIRCLE_NUM = 50;
 	private final static int DELAY = 2000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-
+	private GOval circle;
 	public void run() {
+		addMouseListeners();
 		for(int i = 0; i < CIRCLE_NUM; i++) {
-			GOval circle = new GOval (CIRCLE_D, CIRCLE_D);
+			circle = new GOval (CIRCLE_D, CIRCLE_D);
 			double x = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double y = rgen.nextDouble(0, getHeight() - CIRCLE_D);
 			circle.setFilled(true);
 			add(circle, x, y);
 		}
 		while(true) {
-			GOval circle = new GOval (CIRCLE_D, CIRCLE_D);
+			circle = new GOval (CIRCLE_D, CIRCLE_D);
 			double x = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double y = rgen.nextDouble(0, getHeight() - CIRCLE_D);
 			circle.setFilled(true);
 			add(circle, x, y);
 			pause(DELAY);
+		}
+	}
+	public void mouseClicked(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		int clicks = 0;
+		GObject obj = getElementAt(x, y);
+		for(int i = 0; i < 3; i++) {
+		if(obj != circle) {
+			break;
+		}
+		remove(circle);
 		}
 	}
 
