8b7df6ba1f4d0125c008cbc44545f2c8aa3eaf50
S <s>
2023-11-30 22:54:32 +0400

    {"types":"C","runs":95}

--- Green.java
+++ Green.java
@@ -1,4 +1,21 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class Green {
+public class Green extends GraphicsProgram {
+	private final static int CIRCLE_D = 40;
+	private final static int CIRCLE_NUM = 50;
+	private final static int DELAY = 2000;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+		for(int i = 0; i < CIRCLE_NUM; i++) {
+			GOval circle = new GOval (CIRCLE_D, CIRCLE_D);
+			double x = rgen.nextDouble(0, getWidth());
+			double y = rgen.nextDouble(0, getHeight());
+			add(circle, x, y);
+			pause(DELAY);
+		}
+	}
 
 }
