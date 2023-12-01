7e739a247f09a11a1e5e0fdf7216a7853a706c13
S <s>
2023-12-01 21:04:27 +0400

    {"types":"C","runs":1}

--- CheckBoard.java
+++ CheckBoard.java
@@ -1,4 +1,24 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class CheckBoard {
+public class CheckBoard extends GraphicsProgram{
+	private static final int N_ROWS = 8;
+	private static final int N_COLUMNS = 8;
+	public void run() {
+		drawTheBoard();
+	}
 
+	private void drawTheBoard() {
+		for(int i = 0; i < N_ROWS; i++) {
+			GRect cube = new GRect(getWidth() / N_ROWS, getHeight() / N_COLUMNS);
+			for(int j = 0; j < N_COLUMNS; j++) {
+				int x = 0 + i * N_ROWS;
+				int y = 0 + j * N_COLUMNS;
+				add(cube, x, y);
+			}
+		}
+		
+	}
+
+	
 }
