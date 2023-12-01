04fcccb8200e8a8d27a468f908abde5915e57c3e
S <s>
2023-12-01 23:23:57 +0400

    {"types":"C","runs":31}

--- Result.java
+++ Result.java
@@ -7,13 +7,14 @@ public class Result extends ConsoleProgram {
 		int x = 1;
 		int y = 2;
 		GOval o = new GOval(2, 1); // width, height
-		println(racxa(x, 2, o));
-		println(racxa(y, x / 2 + x, o));
+		//println(racxa(x, 2, o));
+		//println(racxa(y, x / 2 + x, o));
 	}
 
 	public int racxa(int x, int y, GOval o){
 	String s = "hello world";
 	x = varesiRacxa(x/y, x+(int)o.getHeight(), s); //5
+	println(x);
 	o.scale(2);
 	y = varesiRacxa(x, y+(int)o.getHeight(), s);   //7
 	return x+y;
