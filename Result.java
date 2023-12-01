ae1e5a23cc8268a77b2b381f176f3ee3390107f8
S <s>
2023-12-01 23:28:36 +0400

    {"types":"C","runs":36}

--- Result.java
+++ Result.java
@@ -14,6 +14,7 @@ public class Result extends ConsoleProgram {
 	public int racxa(int x, int y, GOval o){
 	String s = "hello world";
 	x = varesiRacxa(x/y, x+(int)o.getHeight(), s); //5
+	println(x);
 	o.scale(2);
 	y = varesiRacxa(x, y+(int)o.getHeight(), s);   //7
 	println(y);
