2e85a2953e105437c15ad6fb24c3f13ac935a7d8
S <s>
2023-12-01 23:13:48 +0400

    {"types":"C","runs":26}

--- Result.java
+++ Result.java
@@ -1,4 +1,26 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class Result {
+public class Result extends GraphicsProgram {
+	public void run() {
+		int x = 1;
+		int y = 2;
+		GOval o = new GOval(2, 1); // width, height
+		println(racxa(x, 2, o));
+		println(racxa(y, x / 2 + x, o));
+	}
 
+	public int racxa(int x, int y, GOval o){
+	String s = "hello world";
+	x = varesiRacxa(x/y, x+(int)o.getHeight(), s); //5
+	o.scale(2);
+	y = varesiRacxa(x, y+(int)o.getHeight(), s);   //7
+	return x+y;
+	}
+
+	public int varesiRacxa(double x, int y, String s) {
+		int z = (int) x + s.charAt(0) - s.charAt(1);
+		s = s.substring(1);
+		return z + y;
+	}
 }
