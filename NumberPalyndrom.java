c64860938768a63bc3d2796243cb97ef2f485f4e
S <s>
2023-12-02 01:05:26 +0400

    {"types":"C","runs":18}

--- NumberPalyndrom.java
+++ NumberPalyndrom.java
@@ -4,13 +4,13 @@ public class NumberPalyndrom extends ConsoleProgram{
 	
 	public void run() {
 		int n = readInt("enter a number: ");
-		int m = n;
 		int powersOfTen = 0;
 		boolean isPalyndrom = true;
 			while(Math.pow(10, (powersOfTen + 1)) <= n) {
 				powersOfTen++;
 			}
 		 for(int i = 0; i <= powersOfTen ; i ++) {
+			 int m = n;
 			 int firstDigit = (int) (n / Math.pow(10,  powersOfTen - i));
 			 n =(int) (n - firstDigit * Math.pow(10, powersOfTen - i));
 			 int lastDigit = (int) (m % Math.pow(10,  powersOfTen - i));
