8b86c5f0accbbaaa0e8da1183f1f5e709f84603f
S <s>
2023-12-01 21:48:17 +0400

    {"types":"C","runs":24}

--- CheckBoard.java
+++ CheckBoard.java
@@ -7,8 +7,8 @@ import acm.program.GraphicsProgram;
 public class CheckBoard extends GraphicsProgram {
 	private static final int N_ROWS = 8;
 	private static final int N_COLUMNS = 8;
-	private GRect lastClickedRect = null;
-	private GRect rect2 = null;
+	private GRect lastClickedRect1 = null;
+	private GRect lastClickedRect2 = null;
 
 	public void run() {
 		drawTheBoard();
@@ -30,14 +30,15 @@ public class CheckBoard extends GraphicsProgram {
 	public void mouseClicked(MouseEvent e) {
 		GRect rect = (GRect) getElementAt(e.getX(), e.getY());
 		if (rect != null) {
-			if (lastClickedRect != null && lastClickedRect.isFilled()) {
-				lastClickedRect.setFilled(false);
+			if (lastClickedRect1 != null && lastClickedRect1.isFilled()) {
+				lastClickedRect1.setFilled(false);
 			}
 			if (!rect.isFilled()) {
 				rect.setFilled(true);
-				lastClickedRect = rect;
+				lastClickedRect1 = rect;
 			} else {
 				rect.setFilled(false);
+				lastClickedRect2 = rect;
 			}
 		}
 	}
