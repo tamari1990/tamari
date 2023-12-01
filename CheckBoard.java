f332815dfd092ae227a39d2f72238a89ceb5d9ff
S <s>
2023-12-01 21:05:36 +0400

    {"types":"C","runs":2}

--- CheckBoard.java
+++ CheckBoard.java
@@ -12,8 +12,8 @@ public class CheckBoard extends GraphicsProgram{
 		for(int i = 0; i < N_ROWS; i++) {
 			GRect cube = new GRect(getWidth() / N_ROWS, getHeight() / N_COLUMNS);
 			for(int j = 0; j < N_COLUMNS; j++) {
-				int x = 0 + i * N_ROWS;
-				int y = 0 + j * N_COLUMNS;
+				int x = 0 + i * getWidth() / N_ROWS;
+				int y = 0 + j * getHeight() / N_COLUMNS;
 				add(cube, x, y);
 			}
 		}
