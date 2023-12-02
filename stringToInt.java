03cc38886c2fad828cd9cb304a1bf892f23a089c
S <s>
2023-12-02 16:39:23 +0400

    {"types":"C","runs":17}

--- stringToInt.java
+++ stringToInt.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class stringToInt {
+public class stringToInt extends ConsoleProgram{
+	
+	public void run() {
+		String s2 = readLine("enter text: ");
+		int n = makeS2Int(s2);
+		println(n);
+	}
+	private int makeS2Int(String s2) {
+		int num = 0;
+		String s3 = "" + num;
+		while (!s2.equals(s3)) {
+			num++;
+			s3 = "" + num;
+		}
+		return num;
+	}
 
 }
