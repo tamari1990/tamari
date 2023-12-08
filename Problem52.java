9237b7ad60bca109aa5c2dfe04a7f57912819a63
S <s>
2023-12-09 01:21:47 +0400

    {"types":"C","runs":1}

--- Problem52.java
+++ Problem52.java
@@ -1,4 +1,27 @@
+import java.io.*;
 
-public class Problem52 {
+import acm.program.ConsoleProgram;
+
+public class Problem52 extends ConsoleProgram {
+
+	public void run() {
+		try {
+			BufferedReader rd = new BufferedReader(new FileReader("52"));
+			while (true) {
+				String line = rd.readLine();
+				if (line == null) {
+					break;
+				}
+				println(line);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			println("File not found");
+		} catch (IOException e) {
+			e.printStackTrace();
+			println("error detected");
+		}
+
+	}
 
 }
