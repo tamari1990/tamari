f43063cc338d2d0880e993de2582fd9d833e6fce
S <s>
2023-12-10 02:25:49 +0400

    {"types":"C","runs":22}

--- Problem55.java
+++ Problem55.java
@@ -7,11 +7,12 @@ public class Problem55 extends ConsoleProgram{
 		int n = readInt("enter n: ");
 		int[] array = new int[n];
 		for(int i = 0; i < n; i++){
-			array[i] = readInt("enter a number :");
+			array[i] = readInt("enter a number : ");
 		}
 		for(int i = 1; i < array.length; i++){
 			if(array[i + 1] < array[i]){
 				isSorted = false;
+				break;
 			}
 		}
 		println(isSorted);
