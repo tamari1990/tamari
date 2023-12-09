fb983adb95fc1f6d6f4b088111d672c72d9d966e
S <s>
2023-12-10 01:11:06 +0400

    {"types":"C","runs":1}

--- Problem53.java
+++ Problem53.java
@@ -1,4 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Problem53 {
+import acm.program.ConsoleProgram;
+
+public class Problem53 extends ConsoleProgram {
+
+	public void run() {
+		try {
+			BufferedReader rd = new BufferedReader(new FileReader("53"));
+			String text = "";
+			while (true) {
+				String newLine = rd.readLine();
+				if (newLine == null) {
+					break;
+				}
+				text = text + newLine + "/n";
+			}
+			rd.close();
+			countStatistics(text);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			println("file not found");
+		} catch (IOException e) {
+			e.printStackTrace();
+			println("some other error detected");
+		}
+	}
+
+	private void countStatistics(String text) {
+		int numOfSymbols = 0;
+		for(int i = 0; i < text.length(); i++){
+			if(text.charAt(i) != ' '){
+				numOfSymbols++;
+			}
+		}
+		println(numOfSymbols);
+		
+	}
 
 }
