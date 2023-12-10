223ff79fb0b8fd4c1dc4da54fe3e6328531822dc
S <s>
2023-12-11 01:08:48 +0400

    {"types":"C","runs":1}

--- Problem56.java
+++ Problem56.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class Problem56 {
+public class Problem56 extends ConsoleProgram {
+
+	public void run() {
+		int n = readInt("enter n: ");
+		int[] array = new int[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = readInt("enter number");
+		}
+		int minimum = findMinumum(array);
+		println(minimum);
+	}
+
+	private int findMinumum(int[] array) {
+		int min = array[1];
+		for(int j = 0; j < array.length; j++){
+			if(array[j] < min){
+				min = array[j];
+			}
+		}
+		return min;
+	}
 
 }
