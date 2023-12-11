9206d8d67f2788187c9e0c1b8ba7d45e36a74e10
S <s>
2023-12-11 22:10:06 +0400

    {"types":"C","runs":21}

--- ArrayListToString.java
+++ ArrayListToString.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
-public class ArrayListToString {
+import acm.program.ConsoleProgram;
 
+public class ArrayListToString extends ConsoleProgram {
+	public void run() {
+		ArrayList<String> myArrayList = new ArrayList<String>();
+		myArrayList.add("hi");
+		myArrayList.add("there");
+		String s = "";
+		for (int i = 0; i < myArrayList.size(); i++) {
+			s += myArrayList.get(i);
+		}
+		println(s);
+	}
 }
