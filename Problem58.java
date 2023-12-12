c449a7d4a38d9df6854a8e690c3cd4cedd170c78
S <s>
2023-12-12 18:01:05 +0400

    {"types":"C","runs":5}

--- Problem58.java
+++ Problem58.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class Problem58 {
+public class Problem58 extends ConsoleProgram{
+	
+	public void run(){
+		int[][] magic = new int[2][2];
+		magic[0] [0] = 1;
+		magic[0] [1] = 2;
+		magic[1] [0] = 3;
+		magic[1] [1] = 4;
+		boolean isMagic = true;
+		
+		
+	}
 
 }
