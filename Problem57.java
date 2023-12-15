a690d32bec13909b868bbca4da64f0368aefbaa8
S <s>
2023-12-15 04:34:50 +0400

    {"types":"C","runs":16}

--- Problem57.java
+++ Problem57.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
 
-public class Problem57 {
+import acm.program.ConsoleProgram;
 
+public class Problem57 extends ConsoleProgram {
+
+	public void run() {
+		ArrayList<Integer> array1 = new ArrayList<Integer>();
+		ArrayList<Integer> array2 = new ArrayList<Integer>();
+		String word1 = readLine("enter the first word: ");
+		String word2 = readLine("enter the second word: ");
+		int[] counts = new int[26];
+		for (int i = 0; i < word1.length(); i++) {
+			char character = word1.charAt(i);
+			int index = character - 'a';
+			counts[index]++;
+		}
+
+	}
 }
