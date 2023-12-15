f4ce44e048196fd705103de8254e05cb59f2fb55
S <s>
2023-12-16 03:52:18 +0400

    {"types":"C","runs":5}

--- Problem58.java
+++ Problem58.java
@@ -4,7 +4,7 @@ public class Problem58 extends ConsoleProgram {
 
 	public void run() {
 		int[][] magic = new int[2][2];
-		magic[0][0] = 1;
+		magic[0][0] = 5;
 		magic[0][1] = 2;
 		magic[1][0] = 3;
 		magic[1][1] = 4;
