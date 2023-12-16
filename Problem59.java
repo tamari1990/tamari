4e90ddf4aeb8ef30789ed3111218ec8e38300a3d
S <s>
2023-12-16 16:50:36 +0400

    {"types":"C","runs":1}

--- Problem59.java
+++ Problem59.java
@@ -1,4 +1,30 @@
+import acm.program.ConsoleProgram;
 
-public class Problem59 {
+public class Problem59 extends ConsoleProgram {
+	
+	public void run(){
+		int[][] matrix = new int[][] {
+			{0,1,1,0,0,1,0,0}, 
+			{0,1,1,1,1,1,0,0},
+			{0,0,1,1,1,1,0,0}, 
+			{0,0,1,1,1,1,0,0},
+		};
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix[0].length; j++){
+				int num = matrix[i][j];
+				if(num == 1){
+					for(int k = i; k < matrix[0].length; k++){
+						if(matrix[k][j] == 1){
+							for(int r = 0; r < matrix.length; r++){
+								if(matrix[i][1 + r] == 1 && matrix[k][1 + r] == 1){
+									println((k - i) * (r));
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
 }
