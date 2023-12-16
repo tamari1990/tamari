3a0f0d6626a487a531ffa6924b2555b5359ddc1b
S <s>
2023-12-16 17:29:40 +0400

    {"types":"C","runs":16}

--- Problem61.java
+++ Problem61.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
-public class Problem61 {
+import acm.program.ConsoleProgram;
+
+public class Problem61 extends ConsoleProgram{
+	
+	public void run(){
+		ArrayList<Integer> myArray = new ArrayList<Integer>();
+		int m = readInt("enter m: ");
+		int n = readInt("enter n: ");
+		for(int i = 0; i < n; i++){
+			int num = readInt("enter a number ");
+			myArray.add(num);
+		}
+		for(int i = 0; i < myArray.size(); i++){
+			int number = myArray.get(i);
+			for(int j = 0; j < myArray.size(); i++){
+				if(myArray.get(j) + number == m && j != i){
+					println(myArray.get(j));
+					println(number);
+				}
+			}
+		}
+	}
 
 }
