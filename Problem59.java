fe916ba7c6a89666307da61cafec39509ee371bb
S <s>
2023-12-16 17:04:05 +0400

    {"types":"C","runs":6}

--- Problem59.java
+++ Problem59.java
@@ -3,8 +3,12 @@ import acm.program.ConsoleProgram;
 public class Problem59 extends ConsoleProgram {
 
 	public void run() {
-		int[][] matrix = new int[][] { { 0, 1, 1, 0, 0, 1, 0, 0 }, { 0, 1, 1, 1, 1, 1, 0, 0 },
-				{ 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, };
+		int[][] matrix = new int[][] {
+            {0,1,1,0,0,1,0,0}, 
+            {0,1,1,1,1,1,0,0},
+            {0,0,1,1,1,1,0,0}, 
+            {0,0,1,1,1,1,0,0},
+        };
 		int maxLength = 0;
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[0].length; j++) {
@@ -15,7 +19,7 @@ public class Problem59 extends ConsoleProgram {
 							for (int r = 0; r < matrix[0].length - j - 1; r++) {
 								if (matrix[i][j + 1 + r] == 1 && matrix[k][j + 1 + r] == 1) {
 									if (((k - i) * (r + 1)) > maxLength) {
-										maxLength = ((k - i) * (r + 1));
+										maxLength = ((k - i + 1) * (r + 1));
 									}
 								}
 							}
