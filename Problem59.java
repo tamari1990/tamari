24b149cda01174ca4d85a060e3f296e36160d8a1
S <s>
2023-12-16 17:10:02 +0400

    {"types":"C","runs":8}

--- Problem59.java
+++ Problem59.java
@@ -3,14 +3,10 @@ import acm.program.ConsoleProgram;
 public class Problem59 extends ConsoleProgram {
 
 	public void run() {
-		int[][] matrix = new int[][] {
-			{0,1,1,1,1,1,1,1,1,1,1,1,0}, 
-			{0,0,1,1,1,1,1,1,1,1,1,0,0},
-			{0,0,0,1,1,1,1,1,1,1,0,0,0}, 
-			{0,0,0,0,1,1,1,1,1,0,0,0,0},
-			{0,0,0,0,0,1,1,1,0,0,0,0,0},
-			{0,0,0,1,1,1,1,1,1,1,1,0,0},
-		};
+		int[][] matrix = new int[][] { { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+				{ 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 }, };
 		int maxLength = 0;
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[0].length; j++) {
@@ -18,18 +14,19 @@ public class Problem59 extends ConsoleProgram {
 				if (num == 1) {
 					for (int k = i; k < matrix.length; k++) {
 						if (matrix[k][j] == 1) {
-							for (int r = 0; r < matrix[0].length - j - 1; r++) {
-								if (matrix[i][j + 1 + r] == 1 && matrix[k][j + 1 + r] == 1) {
-									if (((k - i) * (r + 1)) > maxLength) {
-										maxLength = ((k - i + 1) * (r + 1));
-									}
+							for (int r = 0; j + 1 + r < matrix[0].length && matrix[i][j + 1 + r] == 0
+									&& matrix[k][j + 1 + r] == 0; r++) {
+
+								if (((k - i) * (r + 1)) > maxLength) {
+									maxLength = ((k - i + 1) * (r + 1));
 								}
+
 							}
 						}
 					}
 				}
 			}
+			println(maxLength);
 		}
-		println(maxLength);
 	}
 }
