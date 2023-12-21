372293169812b6483d7e6cbcac0bae06b5386aa7
S <s>
2023-12-21 17:19:57 +0400

    {"types":"R","runs":4}

--- takomiyvarsdzalian.java
+++
@@ -1,9 +0,0 @@
-import acm.program.ConsoleProgram;
-
-public class takomiyvarsdzalian extends ConsoleProgram {
-	public void run(){
-		String n = readLine("enter your name and i will tel you who you are ");
-		println("Yle");
-	}
-
-}
