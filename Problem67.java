cbb1e3ac6a4ec3a19f5f619e2c2982ca4d8b356c
S <s>
2024-01-05 23:35:38 +0400

    {"types":"C","runs":1}

--- Problem67.java
+++ Problem67.java
@@ -1,4 +1,33 @@
+import java.awt.event.ActionEvent;
 
-public class Problem67 {
+import javax.swing.JTextField;
+
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+import acm.util.ErrorException;
+
+public class Problem67 extends GraphicsProgram{
+	
+	private JTextField myTextField;
+	public void init(){
+		myTextField = new JTextField(50);
+		add(myTextField, SOUTH);
+		myTextField.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		String photoAdress = e.getActionCommand();
+		try{
+			GImage image = new GImage(photoAdress);
+			double x = getWidth()/ 2 - image.getWidth() / 2;
+			double y = getHeight()/ 2 - image.getHeight() / 2;
+			add(image, x, y);
+		}catch(Exception ex){
+			add(new GLabel("Invalid adress"), 20, 20);
+			removeAll();
+		}
+		myTextField.setText("");
+	}
 
 }
