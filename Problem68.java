fe61edf15f1b4cd25ed7e8b19edeae8249feeaef
S <s>
2024-01-06 01:21:55 +0400

    {"types":"C","runs":12}

--- Problem68.java
+++ Problem68.java
@@ -1,4 +1,37 @@
+import java.awt.event.ActionEvent;
 
-public class Problem68 {
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class Problem68 extends GraphicsProgram{
+	
+	private JTextField textField;
+	private JButton button;
+	public void init(){
+		textField = new JTextField(20);
+		add(textField, SOUTH);
+		button = new JButton("Enter");
+		add(button, SOUTH);
+		addActionListeners();
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		println("here");
+		if(e.getSource() == textField || e.getSource() == button){
+			String text = textField.getText();
+			println(text);
+			if(text == null || text.equals("")){
+				return;
+			}
+			int x = 40;
+			int y = 40;
+			add(new GLabel(text), x, y);
+			y += 40;
+			textField.setText("");
+		}
+	}
 
 }
