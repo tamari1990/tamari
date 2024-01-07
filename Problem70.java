8e2dbd00d85fa6494a1c029398e40b469ab2700a
S <s>
2024-01-07 17:00:04 +0400

    {"types":"C","runs":2}

--- Problem70.java
+++ Problem70.java
@@ -4,54 +4,55 @@ import java.awt.Color;
 import java.awt.event.ComponentEvent;
 
 public class Problem70 extends GraphicsProgram {
-    private GCompound grid;
-    private GRect shape;
+	private GCompound grid;
+	private GRect shape;
 
-    private int numRows = 10; 
-    private int numCols = 10; 
+	private int numRows = 10;
+	private int numCols = 10;
 
-    public void run() {
-        drawShape();
-        drawGrid();
-    }
+	public void run() {
+		drawShape();
+		drawGrid();
+	}
 
-    public void componentResized(ComponentEvent e) {
-        drawGrid();
-    }
+	public void componentResized(ComponentEvent e) {
+		drawGrid();
+	}
 
-    private void drawShape() {
-        if (shape != null) {
-            remove(shape);
-        }
-        shape = new GRect(200, 100);
-        shape.setFilled(true);
-        shape.setFillColor(Color.BLUE);
-        add(shape, getWidth() / 2.0 - shape.getWidth() / 2.0, getHeight() / 2.0 - shape.getHeight() / 2.0);
-    }
+	private void drawShape() {
+		if (shape != null) {
+			remove(shape);
+		}
+		shape = new GRect(200, 100);
+		shape.setFilled(true);
+		shape.setFillColor(Color.BLUE);
+		add(shape, getWidth() / 2.0 - shape.getWidth() / 2.0, getHeight() / 2.0 - shape.getHeight() / 2.0);
+	}
 
-    private void drawGrid() {
-        if (grid != null) {
-            remove(grid);
-        }
+	private void drawGrid() {
+		if (grid != null) {
+			remove(grid);
+		}
 
-        grid = new GCompound();
+		grid = new GCompound();
 
-        double boxWidth = shape.getWidth() / numCols;
-        double boxHeight = shape.getHeight() / numRows;
+		double boxWidth = shape.getWidth() / numCols;
+		double boxHeight = shape.getHeight() / numRows;
 
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                GRect gridBox = createGridBox(boxWidth, boxHeight);
-                grid.add(gridBox, col * boxWidth, row * boxHeight);
-            }
-        }
+		for (int row = 0; row < numRows; row++) {
+			for (int col = 0; col < numCols; col++) {
+				GRect gridBox = createGridBox(boxWidth, boxHeight);
+				grid.add(gridBox, col * boxWidth + getWidth() / 2 - shape.getWidth() / 2,
+						row * boxHeight + getHeight() / 2 - shape.getHeight() / 2);
+			}
+		}
 
-        add(grid);
-    }
+		add(grid);
+	}
 
-    private GRect createGridBox(double width, double height) {
-        GRect gridBox = new GRect(width, height);
-        gridBox.setColor(Color.LIGHT_GRAY);
-        return gridBox;
-    }
+	private GRect createGridBox(double width, double height) {
+		GRect gridBox = new GRect(width, height);
+		gridBox.setColor(Color.LIGHT_GRAY);
+		return gridBox;
+	}
 }
