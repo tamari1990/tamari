e4afdc315b005cfcf1c3efcd6206cff8c5679da3
S <s>
2024-01-09 00:06:04 +0400

    {"types":"C","runs":1}

--- Buttons.java
+++ Buttons.java
@@ -1,4 +1,21 @@
+import java.awt.event.ActionEvent;
 
-public class Buttons {
+import javax.swing.JButton;
 
+import acm.program.ConsoleProgram;
+
+public class Buttons extends ConsoleProgram{
+
+	public void init(){
+		JButton button = new JButton ("HI");
+		add(button, SOUTH);
+		addActionListeners();
+	}
+	
+	public void ActionPerformed(ActionEvent e){
+		String command = e.getActionCommand();
+		if(command.equals("HI")){
+			println("hello");
+		}
+	}
 }
