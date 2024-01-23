0120953013e64405bdd72494a8fd91f5767a5702
S <s>
2024-01-24 00:37:48 +0400

    {"types":"C","runs":5}

--- ballgame.java
+++ ballgame.java
@@ -1,13 +1,19 @@
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class ballgame extends GraphicsProgram{
 	
-	private static final int BALL_SIZE = 50;
+	private RandomGenerator rgen = new RandomGenerator();
+	private static final int BALL_SIZE = 20;
 	private GOval ball;
 	public void run(){
 		ball = new GOval(BALL_SIZE * 2, BALL_SIZE * 2);
+		ball.setFilled(true);
 		add(ball, getWidth() / 2 - BALL_SIZE, getHeight() / 2 - BALL_SIZE);
+		int vx = rgen.nextInt();
+		int vy = rgen.nextInt();
+		ball.move(vx, vy);
 	}
 
 }
