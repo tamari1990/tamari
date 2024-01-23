524749b8df124f6b63b5e580fb0578ecb72cd826
S <s>
2024-01-23 23:18:32 +0400

    {"types":"C","runs":3}

--- multiply.java
+++ multiply.java
@@ -22,8 +22,8 @@ public class multiply extends ConsoleProgram {
 		ArrayList<Integer>result = new ArrayList<Integer>();
 		int lastDigit = firstNumber.get(firstNumber.size()) * secondNumber.get(secondNumber.size()) % 10;
 		result.add(lastDigit);
-		for (int i = firstNumber.size(); i >= 0; i--) {
-			for (int j = secondNumber.size(); j >= 0; j--) {
+		for (int i = firstNumber.size(); i > 1; i--) {
+			for (int j = secondNumber.size(); j > 1; j--) {
 				
 				int digit = (firstNumber.get(i) * secondNumber.get(j - 1) % 10
 						+ firstNumber.get(i) * secondNumber.get(j) / 10 + firstNumber.get(i - 1) * secondNumber.get(j) % 10);
@@ -39,7 +39,7 @@ public class multiply extends ConsoleProgram {
 	private ArrayList<Integer> turnAround(ArrayList<Integer> result) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> reversed = new ArrayList<Integer>();
-		for(int i = result.size(); i >= 0; i--){
+		for(int i = result.size(); i > 1; i--){
 			reversed.add(result.get(i));
 		}
 		
