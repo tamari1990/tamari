0408f9c3c9a158d11b26dd4c0f33caf0158c6f5b
S <s>
2024-01-24 00:39:17 +0400

    {"types":"C","runs":8}

--- ballgame.java
+++ ballgame.java
@@ -6,7 +6,7 @@ public class ballgame extends GraphicsProgram{
 	
 	private RandomGenerator rgen = new RandomGenerator();
 	private static final int BALL_SIZE = 20;
-	private int DELAY = 10000;
+	private int DELAY = 20;
 	private GOval ball;
 	public void run(){
 		ball = new GOval(BALL_SIZE * 2, BALL_SIZE * 2);
