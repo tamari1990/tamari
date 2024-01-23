7c4914d06d3bef5868fdb1cb15f32e66a563505d
S <s>
2024-01-24 00:54:53 +0400

    {"types":"C","runs":13}

--- ballgame.java
+++ ballgame.java
@@ -1,23 +1,57 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.JTextField;
+
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class ballgame extends GraphicsProgram{
-	
+public class ballgame extends GraphicsProgram {
+
 	private RandomGenerator rgen = new RandomGenerator();
 	private static final int BALL_SIZE = 20;
 	private int DELAY = 20;
 	private GOval ball;
-	public void run(){
+	private JTextField xVelocity;
+	private JTextField yVelocity;
+	private int vx;
+	private int vy;
+
+	public void init() {
+		xVelocity = new JTextField(10);
+		yVelocity = new JTextField(10);
+		add(xVelocity, SOUTH);
+		add(yVelocity, SOUTH);
+		addActionListeners();
+		xVelocity.addActionListener(this);
+		yVelocity.addActionListener(this);
+	}
+
+	public void run() {
 		ball = new GOval(BALL_SIZE * 2, BALL_SIZE * 2);
 		ball.setFilled(true);
 		add(ball, getWidth() / 2 - BALL_SIZE, getHeight() / 2 - BALL_SIZE);
-		int vx = rgen.nextInt(10);
-		int vy = rgen.nextInt(10);
-		while(true){
-		ball.move(vx, vy);
-		pause(DELAY);
+		vx = rgen.nextInt(10);
+		vy = rgen.nextInt(10);
+		while (true) {
+			ball.move(vx, vy);
+			pause(DELAY);
+
+			// Check for collisions with the edges of the window
+			if (ball.getX() + 2 * BALL_SIZE >= getWidth() || ball.getX() <= 0) {
+				vx = -vx;
+			}
 		}
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == xVelocity){
+			String v1 = xVelocity.getText();
+			vx = Integer.parseInt(v1);
+		}
+		if(e.getSource() == yVelocity){
+			String v2 = yVelocity.getText();
+			vy = Integer.parseInt(v2);
+		}
+	}
 }
