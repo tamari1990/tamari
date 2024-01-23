72b8230394208122a4106e5156405471b537447d
S <s>
2024-01-24 00:34:27 +0400

    {"types":"C","runs":4}

--- ballgame.java
+++ ballgame.java
@@ -1,4 +1,13 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class ballgame {
+public class ballgame extends GraphicsProgram{
+	
+	private static final int BALL_SIZE = 50;
+	private GOval ball;
+	public void run(){
+		ball = new GOval(BALL_SIZE * 2, BALL_SIZE * 2);
+		add(ball, getWidth() / 2 - BALL_SIZE, getHeight() / 2 - BALL_SIZE);
+	}
 
 }
