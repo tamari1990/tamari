43cb1800ed3a0670b004e7d5bdebbd48ef056adb
S <s>
2024-01-24 21:34:07 +0400

    {"types":"C","runs":1}

--- AddSpaces.java
+++ AddSpaces.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
-public class AddSpaces {
+import acm.program.ConsoleProgram;
+
+public class AddSpaces extends ConsoleProgram{
+	
+	public void init(){
+		String sentence = "EnjoyYourExam";
+		ArrayList<Integer> indexes = new ArrayList<Integer>();
+		sentence = addSpaces(sentence, indexes);
+		println(sentence);
+	}
+
+	private String addSpaces(String sentence, ArrayList<Integer> indexes) {
+		String newSentence = "";
+		for(int i = 0; i < indexes.size(); i++){
+			int index = indexes.get(i);
+			newSentence += sentence.substring(index);
+		}
+		return newSentence;
+	}
 
 }
