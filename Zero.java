2d2cc29f355320d67ba0c2a29e79a6e0ec72a27d
S <s>
2024-01-24 15:55:22 +0400

    {"types":"C","runs":3}

--- Zero.java
+++ Zero.java
@@ -4,7 +4,7 @@ public class Zero extends ConsoleProgram {
 
 	public void init() {
 		int[][] matrix = { { 1, 0, 3, 4 }, { 5, 6, 7, 8 }, { 9, 0, 1, 2 } };
-		matrix = nullTheMatrix(matrix);
+		//matrix = nullTheMatrix(matrix);
 		println(matrix);
 	}
 
