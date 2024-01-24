9f0d55c6caf2e8307364532f7139b83b5ec76d91
S <s>
2024-01-24 15:55:44 +0400

    {"types":"C","runs":4}

--- Zero.java
+++ Zero.java
@@ -3,7 +3,11 @@ import acm.program.ConsoleProgram;
 public class Zero extends ConsoleProgram {
 
 	public void init() {
-		int[][] matrix = { { 1, 0, 3, 4 }, { 5, 6, 7, 8 }, { 9, 0, 1, 2 } };
+		int[][] matrix = { 
+				{ 1, 0, 3, 4 },
+				{ 5, 6, 7, 8 },
+				{ 9, 0, 1, 2 }
+				};
 		//matrix = nullTheMatrix(matrix);
 		println(matrix);
 	}
