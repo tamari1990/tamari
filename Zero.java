2674ed80094ec0509fbeec47fec144494edc7a73
S <s>
2024-01-24 15:54:55 +0400

    {"types":"C","runs":2}

--- Zero.java
+++ Zero.java
@@ -4,7 +4,24 @@ public class Zero extends ConsoleProgram {
 
 	public void init() {
 		int[][] matrix = { { 1, 0, 3, 4 }, { 5, 6, 7, 8 }, { 9, 0, 1, 2 } };
-		println(matrix[1][3]);
+		matrix = nullTheMatrix(matrix);
+		println(matrix);
+	}
+
+	private int[][] nullTheMatrix(int[][] matrix) {
+		for(int row = 0; row < matrix.length; row++){
+			for(int column = 0; column < matrix[0].length; column++){
+				if(matrix[row][column] == 0){
+					for(int c = 0; c < matrix[0].length; c++){
+						matrix[row][c] = 0;
+					}
+					for(int r = 0; r < matrix.length; r++){
+						matrix[r][column] = 0;
+					}
+				}
+			}
+		}
+		return matrix;
 	}
 
 }
