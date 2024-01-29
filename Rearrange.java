e3791c893fd43bca0a6fc4a345ed9a385d8ca6b9
S <s>
2024-01-29 21:19:18 +0400

    {"types":"C","runs":1}

--- Rearrange.java
+++ Rearrange.java
@@ -1,4 +1,53 @@
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-public class Rearrange {
+import acm.program.ConsoleProgram;
+
+public class Rearrange extends ConsoleProgram {
+
+	public void init() {
+		String sentence = "keep calm and code on";
+		String arrangedSentence = arrangeTheSentence(sentence);
+		println(arrangedSentence);
+	}
+
+	private String arrangeTheSentence(String sentence) {
+		ArrayList<String> words = new ArrayList<String>();
+		StringTokenizer tokenizer = new StringTokenizer(sentence);
+		while (tokenizer.hasMoreElements()) {
+			String token = tokenizer.nextToken();
+			words.add(token);
+		}
+		int shortestLength = Integer.MAX_VALUE;
+		ArrayList<String> arrangedSentence = new ArrayList<String>();
+		for (int i = 0; i < words.size(); i++) {
+			String shortestWord = findTheShortestWord(words, shortestLength);
+			arrangedSentence.add(shortestWord);
+			int shortestWordsIndex = 0;
+			for (int j = 0; j < words.size(); j++) {
+				if (words.get(i) == shortestWord) {
+					shortestWordsIndex = i;
+				}
+			}
+			words.remove(shortestWordsIndex);
+		}
+		String newSentence = "";
+		for (String newSentencesWords : arrangedSentence) {
+			newSentence += newSentencesWords;
+			newSentence += " ";
+		}
+		return newSentence;
+	}
+
+	private String findTheShortestWord(ArrayList<String> words, int shortestLength) {
+		String shortestWord = "";
+		for (String word : words) {
+			if (word.length() < shortestLength) {
+				shortestLength = word.length();
+				shortestWord = word;
+			}
+		}
+		return shortestWord;
+	}
 
 }
